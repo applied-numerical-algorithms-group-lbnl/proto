b3d1b377350b5988671cfc04cad7c269adc3e815
Dan Graves <dtgraves@lbl.gov>
2018-10-26 14:22:56 -0400

    Euler BCs needed to use copy instead of operator()
    Former-commit-id: cb8df9925ce4b821e51bd83261d332400b7198aa

--- examples/Euler/src/EulerRK4.cpp
+++ examples/Euler/src/EulerRK4.cpp
@@ -43,27 +43,38 @@ EulerRK4Op::operator()(
                        EulerState& a_State)
 {
     Bx dbx0 = a_State.m_dbx0;
+    Bx domain = dbx0;
     Bx dbx = dbx0.grow(NGHOST);
     BoxData<double,NUMCOMPS> U_ave(dbx);
     a_State.m_U.copyTo(U_ave);
     U_ave += a_DX.m_DU;
-    for (auto iter = dbx.begin(); iter != dbx.end(); ++iter)
+
+    int numghost = NGHOST;
+    int numcomps = NUMCOMPS;
+    for(int idir = 0; idir < DIM; idir++)
     {
-        Point pt = *iter;
-        if (!dbx0.contains(pt))
-        {
-            Point pt0 = pt;
-            for (int dir = 0; dir < DIM; dir++)
-            {
-                pt0[dir] = dbx0.low()[dir] 
-                    + (pt[dir] - dbx0.low()[dir])%dbx0.size(dir);
-            }
-            for (int k = 0; k < NUMCOMPS; k++)
-            {
-                U_ave(pt,k) = U_ave(pt0,k);
-            }
-        }
+      Point dirlo = -Point::Basis(idir);
+      Point dirhi =  Point::Basis(idir);
+      Bx edgebxlo = domain.edge(dirlo);
+      Bx edgebxhi = domain.edge(dirhi);
+
+      Bx dstbxlo = edgebxlo.extrude(idir,-(numghost-1));
+      Bx dstbxhi = edgebxhi.extrude(idir, (numghost-1));
+
+      //these are swapped because you have to move in different 
+      //directions to get the periodic image
+      Point shifthi = dirlo*domain.size(idir);
+      Point shiftlo = dirhi*domain.size(idir);
+
+      Bx srcbxlo = dstbxlo.shift(shiftlo);
+      Bx srcbxhi = dstbxhi.shift(shifthi);
+
+      U_ave.copy(U_ave, srcbxlo, 0, dstbxlo, 0, numcomps);
+      U_ave.copy(U_ave, srcbxhi, 0, dstbxhi, 0, numcomps);
+
     }
+
+
     double velmax = EulerOp::step(a_DX.m_DU,U_ave,a_State.m_dbx0);
     a_State.m_velSave = std::max(a_State.m_velSave,velmax);
     a_DX.m_DU *= a_dt;
