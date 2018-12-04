47cb817109bebf9d2eafd45b8ef5294a9b1493a6
Dan Graves <dtgraves@lbl.gov>
2018-12-04 17:33:40 -0500

    fixed integer overflow problem in stencil applications mflop calculation.   added device sync to applyKernel test
    Former-commit-id: 8442f07ae0970194db8f9dd7495143e4c3aac934

--- examples/applyKernel/exec/applyKernel.cpp
+++ examples/applyKernel/exec/applyKernel.cpp
@@ -42,8 +42,8 @@ parseCommandLine(int & a_nx, int & a_numapplies, int argc, char* argv[])
 void
 applyStuff(int  a_nx, int a_numapplies)
 {
-  PR_TIME("applyStuff");
 
+  PR_TIME("whole test");
   Point lo = Point::Zeros();
   Point hi = Point::Ones(a_nx - 1);
   Box domain(lo, hi);
@@ -56,9 +56,14 @@ applyStuff(int  a_nx, int a_numapplies)
 #endif
   Point ghostPt = hiOrderLap.ghost();
   Box   ghostBx = domain.grow(ghostPt);
+
   
-  BoxData<double> phi(ghostBx);
-  BoxData<double> lap(domain);
+  BoxData<double> phi,lap;
+  {
+    PR_TIME("dataholder definition");
+    phi.define(ghostBx);
+    lap.define(domain);
+  }
   
   //remember this is just for timings
   phi.setVal(0.);
@@ -70,6 +75,12 @@ applyStuff(int  a_nx, int a_numapplies)
     PR_TIME("STD  laplacian");
     loOrderLap.apply(phi, lap, domain, true, 1.0/(dx*dx));
   }
+#ifdef PROTO_CUDA
+  {
+    PR_TIME("device sync");
+    cudaDeviceSynchronize();
+  }
+#endif  
   cout << "apply dense laplacian " << a_numapplies << " times" << endl;
   for(int iapp = 0; iapp < a_numapplies; iapp++)
   {
