b72101f10e4d9b8a3a04520b4ae39d2dea6366f6
Dan Graves <dtgraves@lbl.gov>
2018-12-06 14:57:38 -0500

    made benchmark scripts a bit more useful
    Former-commit-id: ee21c4f7a189d2c312d820341cbe29570a47ea09

---
+++ examples/Multigrid/exec/run_fullsolves.sh
@@ -0,0 +1,34 @@
+#!/bin/csh -f
+if ($#argv != 4) then
+  echo "Usage: kernel.sh dirname min_nx max_nx max_iter"
+  exit 1
+endif
+set dirname = $1
+set min_nx  = $2
+set max_nx  = $3
+set max_iter = $4
+
+echo "max_nx = $max_nx, min_nx = $min_nx, dirname = $dirname"
+
+if (! -e $dirname) then
+    set command = "mkdir $dirname"
+    echo $command
+    $command
+endif
+
+set nx_cur = $min_nx
+while ($nx_cur <= $max_nx)
+  set outfile = "$dirname/kernel$nx_cur.out"
+  set command = "./Multigrid.exe -n $nx_cur -m $max_iter >&  $outfile"
+  echo $command
+  $command
+  set command = "mv proto.time.table $dirname/fullsolve.nx.$nx_cur.time.table"
+  echo $command
+  $command
+
+  set nx_cur = `expr 2 \* $nx_cur`
+end
+
+exit 0
+
+
