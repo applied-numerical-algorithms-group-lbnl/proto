9fa5d4628771efa5b4c60ecd90a9838759f4739a
Dan Graves <dtgraves@lbl.gov>
2018-12-05 19:39:38 -0500

    got code back to working.
    Former-commit-id: 6b81ad21cfc94ac68edccee94cad512b6673df6e

--- examples/Multigrid/exec/relaxonly.sh
+++ examples/Multigrid/exec/relaxonly.sh
@@ -1,14 +1,14 @@
 #!/bin/csh
-./Multigrid.exe -n 64  -m 100 | tee  multigrid.out
+./Multigrid.exe -n 64 -r 1 -m 100 | tee  multigrid.out
 mv proto.time.table _multigrid_bench/time.table.64
 mv multigrid.out    _multigrid_bench/screen.out.64
-./Multigrid.exe -n 128  -m 100 | tee  multigrid.out
+./Multigrid.exe -n 128 -r 1 -m 100 | tee  multigrid.out
 mv proto.time.table _multigrid_bench/time.table.128
 mv multigrid.out    _multigrid_bench/screen.out.128
-./Multigrid.exe -n 256  -m 100 | tee  multigrid.out
+./Multigrid.exe -n 256 -r 1 -m 100 | tee  multigrid.out
 mv proto.time.table _multigrid_bench/time.table.256
 mv multigrid.out    _multigrid_bench/screen.out.256
-./Multigrid.exe -n 512  -m 100 | tee  multigrid.out
+./Multigrid.exe -n 512 -r 1 -m 100 | tee  multigrid.out
 mv proto.time.table _multigrid_bench/time.table.512
 mv multigrid.out    _multigrid_bench/screen.out.512
 
