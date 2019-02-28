00f5c988eb47b436497ce169bdbb18b765bdaa8b
cgebhart <cgebhart@lbl.gov>
2019-02-28 14:15:51 -0800

    AMRFAS now has the correct convergence characteristics, but may contain a bug related to boundaries at corners. General code cleanup and consolidation.
    Former-commit-id: 8607bbdba636fdc55d10b7eedda2d2e3e5a3904f

--- examples/CHAMRFAS/exec/test.cpp
+++ examples/CHAMRFAS/exec/test.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
     {
         cout << "Usage: " << endl; 
         cout << "\t Argument 1: Choose one of the following tests to run: " << endl;
-        cout << "\t\tTest 0: Interp Testing" << endl;
+        cout << "\t\tTest 0: Misc Testing (not guaranteed to do anything useful)" << endl;
         cout << "\t\tTest 1: AMR Multigrid" << endl;
         cout << "\t\tTest 2: AMR Operator" << endl;
         cout << "\t\tTest 3: Multigrid" << endl;
@@ -48,7 +48,7 @@ int main(int argc, char** argv)
     // Misc Testing
     if (TEST == 0)
     {
-        //Real L = 2.0*M_PI;
+        cout << "Currently Testing: Nothing" << endl;
         Real L = domainSize;
         int numIter = 1;
         Real error[numIter];
@@ -71,124 +71,11 @@ int main(int argc, char** argv)
             coarsen(coarseTemp, fineLayout, AMR_REFRATIO);
             auto fiter = fineLayout.dataIterator();
             auto citer = coarseLayout.dataIterator();
-
-            OP op(fineLayout, dx);
-
-            LevelData<DATA> PhiF(fineLayout, 1, Proto::Point::Ones());
-            LevelData<DATA> FluxX(fineLayout, 1, Proto::Point::Ones());
-            LevelData<DATA> TrueFluxX(fineLayout, 1, Proto::Point::Ones());
-            LevelData<DATA> FluxY(fineLayout, 1, Proto::Point::Ones());
-            LevelData<DATA> AvgFluxX(coarseTemp, 1, Proto::Point::Ones());
-            LevelData<DATA> AvgFluxY(coarseTemp, 1, Proto::Point::Ones());
-            LevelData<DATA> LPhiF(fineLayout, 1);
-            for (fiter.begin(); fiter.ok(); ++fiter)
-            {
-                BD data = PhiF[fiter];
-                BD trueFlux = TrueFluxX[fiter];
-                data.setVal(0);
-                Proto::Box patchBox = fineLayout[fiter];
-                Proto::forallInPlace_p([=] PROTO_LAMBDA (Proto::Point& a_p, V& a_data)
-                        {
-                        Real x0 = dx*a_p[0];
-                        Real x1 = x0 + dx;
-                        a_data(0) = (sin(x1) - sin(x0))/dx;
-                        //a_data(0) = 4;
-                        }, patchBox, data);
-                Proto::forallInPlace_p([=] PROTO_LAMBDA (Proto::Point& a_p, V& a_data)
-                        {
-                            a_data(0) = -sin(dx*a_p[0]);
-                        },trueFlux);
-            }
-            PhiF.exchange();
-            for (fiter.reset(); fiter.ok(); ++fiter)
-            {
-                BD phi = PhiF[fiter];
-                BD lphi = LPhiF[fiter];
-                BD flux_x = FluxX[fiter];
-                BD flux_y = FluxY[fiter];
-                op(lphi, phi);
-            }
-            op.flux(FluxX, PhiF, 0);
-            op.flux(FluxY, PhiF, 1);
-            FluxX.exchange();
-            FluxY.exchange();
             
-            auto avgx = 0.5*Proto::Shift::Zeros() + 0.5*Proto::Shift::Basis(0);
-            avgx.srcRatio() = Proto::Point::Ones(2);
-            auto avgy = 0.5*Proto::Shift::Zeros() + 0.5*Proto::Shift::Basis(1);
-            avgy.srcRatio() = Proto::Point::Ones(2);
-
-            for (fiter.reset(); fiter.ok(); ++fiter)
-            {
-                BD fluxX = FluxX[fiter];
-                BD avgFluxX = AvgFluxX[fiter];
-                avgFluxX.setVal(0);
-                avgFluxX |= avgy(fluxX,coarseTemp[fiter], 1.0/cdx);
-                
-                BD fluxY = FluxY[fiter];
-                BD avgFluxY = AvgFluxY[fiter];
-                avgFluxY.setVal(0);
-                avgFluxY |= avgx(fluxY, coarseTemp[fiter], 1.0/cdx);
-            }
-
-            LevelData<DATA> FluxRegister(coarseLayout, 1, Proto::Point::Zeros()); //surrogate for RC
-            LevelData<DATA> PhiC(coarseLayout, 1, Proto::Point::Ones());
-            for (citer.begin(); citer.ok(); ++citer)
-            {
-                BD fr = FluxRegister[citer];
-                BD phic = PhiC[citer];
-                fr.setVal(0);
-                phic.setVal(0);
-            }
-            LevelFluxRegister LFR;
-            LFR.define(fineLayout, coarseLayout, fineLayout.physDomain(), AMR_REFRATIO, 1);
-
-            writeLevel(PhiF, "PhiF.test.hdf5");
-            writeLevel(LPhiF, "LPhiF.test.hdf5");
-            writeLevel(PhiC, "PhiC.test.hdf5");
-            writeLevel(FluxX, "FluxX.test.hdf5");
-            writeLevel(TrueFluxX, "TrueFluxX.test.hdf5");
-            writeLevel(FluxY, "FluxY.test.hdf5");
-            writeLevel(AvgFluxX, "AvgFluxX.test.hdf5");
-            writeLevel(AvgFluxY, "AvgFluxY.test.hdf5");
-
-            op.reflux(FluxRegister, PhiC, PhiF, LFR);
-
-            for (citer.reset(); citer.ok(); ++citer)
-            {
-                BD data = FluxRegister[citer];
-                Proto::Box patchBox = coarseLayout[citer];
-                Proto::Box intersect = patchBox & coarseFineDomain;
-                Proto::forallInPlace([=] PROTO_LAMBDA (V& a_data)
-                        {
-                        a_data(0) = 0;
-                        }, intersect, data);
-            }
-            writeLevel(FluxRegister, "FluxRegister.test.hdf5");
-
-            Real lphi = integrate(LPhiF, dx);
-            Proto::Box halo = coarseFineDomain.grow(1);
-            Real flux = 0.0;
-            for (citer.reset(); citer.ok(); ++citer)
-            {
-                BD data = FluxRegister[citer];
-                Proto::Box b = coarseLayout[citer];
-                for (auto iter = b.begin(); iter != b.end(); ++iter)
-                {
-                    if (halo.contains(*iter) && (!coarseFineDomain.contains(*iter)))
-                    {
-                        flux += data(*iter)*cdx*cdx;
-                    }
-                }
-            }
-
-            //Real flux = integrate(FluxRegister, cdx);
-
-            cout << "Integral of LPhiF: " << integrate(LPhiF, dx) << endl;
-            cout << "Integral of FluxRegister: " << integrate(FluxRegister, cdx) << endl;
-            cout << "Error: " << flux + lphi << endl;
-            cout << "cdx/Error: " << cdx/(flux+lphi) << endl;
-            error[n] = flux + lphi;
+            // Define LevelData / AMR Hierarchies
+            // Initialize data
+            // Do computation
+            // Compute error[n]
             domainSize *= 2;
         }
         for (int ii = 1; ii < numIter; ii++)
@@ -201,27 +88,31 @@ int main(int argc, char** argv)
     // AMRFAS Test
     if (TEST == 1)
     {
-        cout << pow(0.5,DIM) << endl;
-        int numLevels = 1;
-        bool do_nesting = false;
+        cout << "Testing full AMRFAS algorithm" << endl;
+        int numLevels = 2;
+        bool do_nesting = true;
 
-        Real cdx = 2.0*M_PI/domainSize;
+        Real L = 2.0*M_PI;
+        Real cdx = L/domainSize;
         std::vector<DisjointBoxLayout> Layouts;
         
         std::vector<std::shared_ptr<LevelData<DATA>>> Phi;
         std::vector<std::shared_ptr<LevelData<DATA>>> Src;
         std::vector<std::shared_ptr<LevelData<DATA>>> Rhs;
         std::vector<std::shared_ptr<LevelData<DATA>>> Res;
-        std::vector<std::shared_ptr<LevelData<DATA>>> Const;
 
         Layouts.resize(numLevels);
         Phi.resize(numLevels);
         Src.resize(numLevels);
         Rhs.resize(numLevels);
         Res.resize(numLevels);
-        Const.resize(numLevels);
 
         Real dx = cdx;
+        cout << "\tNumber of AMR Levels: " << numLevels << endl;
+        cout << "\tReal size of domain: " << L << endl;
+        cout << "\tNested domains: " << do_nesting << endl;
+        cout << "\tTest Problem: Laplacian(<phi>) = <cos(x)>" << endl;
+        cout << "\tInitial Phi: 0" << endl;
         for (int ii = 0; ii < numLevels; ii++)
         {
             auto& layout = Layouts[ii];
@@ -234,7 +125,7 @@ int main(int argc, char** argv)
                     domain = domain.shift(Proto::Point::Ones(domainSize*0.5*(1-1.0/s)));
                 }
                 domain = domain.refine(s);
-                std::cout << "Domain of level " << ii << ": " << domain << std::endl;
+                std::cout << "\t\tDomain of level " << ii << ": " << domain << std::endl;
                 if (ii == 0)
                 {
                     buildLayout(layout, domain, Proto::Point::Ones());
@@ -245,54 +136,46 @@ int main(int argc, char** argv)
                 int s = ipow(AMR_REFRATIO,ii);
                 Box domain = Proto::Box::Cube(domainSize);
                 domain = domain.refine(s);
-                std::cout << "Domain of level " << ii << ": " << domain << std::endl;
+                std::cout << "\t\tDomain of level " << ii << ": " << domain << std::endl;
                 buildLayout(layout, domain, Proto::Point::Ones());
             }
+            std::cout << endl;
             Phi[ii] = std::make_shared<LevelData<DATA>>(layout, 1, Proto::Point::Ones(1));
             Src[ii] = std::make_shared<LevelData<DATA>>(layout, 1, Proto::Point::Ones(1));
             Rhs[ii] = std::make_shared<LevelData<DATA>>(layout, 1, Proto::Point::Zeros());
             Res[ii] = std::make_shared<LevelData<DATA>>(layout, 1, Proto::Point::Zeros());
-            Const[ii] = std::make_shared<LevelData<DATA>>(layout, 1, Proto::Point::Ones());
             auto& phi = *(Phi[ii]);
             auto& src = *(Src[ii]);
             auto& rhs = *(Rhs[ii]);
             auto& res = *(Res[ii]);
-            auto& con = *(Const[ii]);
             auto iter = phi.dataIterator();
             for (iter.reset(); iter.ok(); ++iter)
             {
                 BD phi_i = phi[iter];
                 BD rhs_i = rhs[iter];
                 BD res_i = res[iter];
-                BD con_i = con[iter];
-                con_i.setVal(3);
                 res_i.setVal(0);
                 forallInPlace_p(
-                        [=] PROTO_LAMBDA (Proto::Point& a_pt, Proto::Var<Real>& a_phi)
-                        {
-                        //Real x0 = a_pt[0]*dx;
-                        //Real x1 = a_pt[0]*dx + dx;
-                        //a_phi(0) = -0.5*(sin(x1) - sin(x0))/dx;
-                        //a_phi(0) += 0.1*(sin(5*x1)/5 - sin(5*x0)/5)/dx;
+                    [=] PROTO_LAMBDA (Proto::Point& a_pt, Proto::Var<Real>& a_phi)
+                    {
                         a_phi(0) = 0.0; 
-                        }, phi_i);
+                    }, phi_i);
                 forallInPlace_p(
-                        [=] PROTO_LAMBDA (Proto::Point& a_pt, Proto::Var<Real>& a_rhs)
-                        {
+                    [=] PROTO_LAMBDA (Proto::Point& a_pt, Proto::Var<Real>& a_rhs)
+                    {
                         Real x0 = a_pt[0]*dx;
                         Real x1 = a_pt[0]*dx + dx;
-                        a_rhs(0) = (sin(x1) - sin(x0))/dx;
-                        }, rhs_i);
+                        a_rhs(0) = (sin(x1) - sin(x0))/dx; // = < cos(x) >
+                    }, rhs_i);
             }
             phi.exchange();
             phi.copyTo(src);
             dx /= AMR_REFRATIO;
         } //end initialization
         
-        cout << "Error of Integral of Const: " << integrate(Const, cdx) - pow(2*M_PI, DIM)*3.0 << endl;
-
-        AMRFAS<OP,DATA> amr_op(Layouts, dx*AMR_REFRATIO , numLevels-1, 1);
+        AMRFAS<OP,DATA> amr_op(Layouts, dx*AMR_REFRATIO , numLevels-1, log2(1.0*domainSize) - 1);
         amr_op.write(Src, "AMR_Src.hdf5");
+        amr_op.write(Rhs, "AMR_Rhs.hdf5");
         cout << "Integral of RHS: " << integrate(Rhs, cdx) << endl; 
         cout << "Integral of Res: " << integrate(Res, cdx) << endl; 
         cout << "Integral of Phi: " << integrate(Phi, cdx) << endl; 
@@ -301,13 +184,11 @@ int main(int argc, char** argv)
             amr_op.write(Res, "AMR_Res.%i.hdf5", nn);
             amr_op.write(Phi, "AMR_Phi.%i.hdf5", nn);
             amr_op.vcycle(Phi, Rhs, Res);
-            cout << "Residual: Max = " << absMax(Res) << " Integral = " << integrate(Res, cdx) << endl;
+            cout << "Residual: Max = " << absMax(Res) << "\t\tIntegral = " << integrate(Res, cdx) << endl;
         }
         amr_op.write(Res, "AMR_Res.%i.hdf5", numIter);
         amr_op.write(Phi, "AMR_Phi.%i.hdf5", numIter);
 
-        amr_op.write(Rhs, "AMR_Rhs.hdf5");
-        amr_op.write(Phi, "AMR_Phi.hdf5");
         for (int level = 0; level < numLevels; ++level)
         {
             auto& rhs = *(Rhs[level]);
@@ -319,7 +200,7 @@ int main(int argc, char** argv)
                 BD srcPatch = src[iter];
                 BD phiPatch = phi[iter];
                 BD rhsPatch = rhs[iter];
-                rhsPatch += phiPatch;
+                rhsPatch += phiPatch; //because the solution for phi is -<cos(x)> = -rhs
                 phiPatch -= srcPatch;
             }
         }
@@ -336,9 +217,12 @@ int main(int argc, char** argv)
     // AMR Operator Test
     if (TEST == 2)
     {
+        cout << "Testing Simple AMROperator" << endl;
         const int numLevels = 2;
         Real ei[numIter];
         Real eb[numIter];
+        cout << "\tNumber of AMR Levels: " << numLevels << endl;
+        cout << "\tNested Domains: " << true << endl; // no other option at the moment
         for (int n = 0; n < numIter; n++)
         {
             Real cdx = 2.0*M_PI/domainSize;
@@ -470,17 +354,36 @@ int main(int argc, char** argv)
         LevelData<DATA> R(layout, NUMCOMPS, IntVect::Zero);
         LevelData<DATA> S(layout, NUMCOMPS, IntVect::Zero);
 
-        OP::initialCondition(U,dx);
-        OP::forcing(F,dx);
-        OP::solution(S,dx);
+        //OP::initialCondition(U,dx);
+        //OP::forcing(F,dx);
+        //OP::solution(S,dx);
+
+        auto iter = layout.dataIterator();
+        for (iter.begin(); iter.ok(); ++iter)
+        {
+            BD u = U[iter];
+            u.setVal(0);
+            BD f = F[iter];
+            Proto::forallInPlace_p([=] PROTO_LAMBDA (Proto::Point& a_p, Proto::Var<Real>& a_data)
+            {
+                Real x0 = a_p[0]*dx;
+                Real x1 = x0 + dx;
+                a_data(0) = (sin(x1) - sin(x0))/dx;
+            }, f);
+            BD s = S[iter];
+            Proto::forallInPlace_p([=] PROTO_LAMBDA (Proto::Point& a_p, Proto::Var<Real>& a_data)
+            {
+                Real x = a_p[0]*dx + dx/2;
+                a_data(0) = -cos(x);
+            }, s);
+            BD r = R[iter];
+            r.setVal(0);
+        }
 
         Multigrid<OP, DATA> mg(layout, dx, numLevels-1, false);
         int numIter = 20;
         double resnorm = 0.0;
-        //char fileName[100];
-        //char fileNameU[100];
         TestOp<FArrayBox> op(layout,dx);
-        //fileNum = 0;
         for (int ii = 0; ii < numIter; ii++)
         {
             mg.vcycle(U,F); 
@@ -493,19 +396,11 @@ int main(int argc, char** argv)
 #if CH_MPI
             }
 #endif
-            /*
-            sprintf(fileName,"ResV.%i.hdf5",fileNum);
-            sprintf(fileNameU,"ResU.%i.hdf5",fileNum);
-            writeLevelname(&R,fileName);
-            writeLevelname(&U,fileNameU);
-            fileNum++;
-            */
         }
-        auto iter = layout.dataIterator();
         double umax = 0.0;
         double umin = 0.0;
         double error = 0.0;
-        for (iter.begin(); iter.ok(); ++iter)
+        for (iter.reset(); iter.ok(); ++iter)
         {
             Proto::BoxData<double> s = S[iter()];
             Proto::BoxData<double> u = U[iter()];
