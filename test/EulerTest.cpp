2fbb3cbd74dffa35139e1a4fd08dc58abfbedbc5
Eddie Davis <eddiedavis@u.boisestate.edu>
2019-06-10 17:48:01 -0700

    Added EulerTest benchmark class.
    Former-commit-id: fd4d5b02e81177ded012f4b83d78fffbb6cdc70b

---
+++ test/EulerTest.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using std::string;
+using std::to_string;
+#include <gtest/gtest.h>
+using namespace testing;
+
+#include <BenchmarkTest.hpp>
+#include "Proto_DataFlow.H"
+
+namespace test {
+    class EulerTest : public BenchmarkTest {
+
+    };
+}
