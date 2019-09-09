d15eee861a0769c1912748a5eb43c18d973499ce
dtgraves <dtgraves@lbl.gov>
2019-09-09 12:22:48 -0700

    EBforall_p now works on the host
    Former-commit-id: 8faa51849f2b1412b9196e18a5e9c7e78dd70eab

--- experimental/EBForAll/EBForAll.cpp
+++ experimental/EBForAll/EBForAll.cpp
@@ -21,13 +21,14 @@ getIrregData(EBBoxData<cent, data_t, ncomp>& a_s)
 }
 
 
-template< typename data_t, unsigned int ncomp>
+template<CENTERING cent,  typename data_t, unsigned int ncomp>
 struct
 uglyStruct
 {
-  data_t*      m_startPtr;
-  unsigned int m_varsize;
-  unsigned int m_index;
+  data_t*        m_startPtr;
+  unsigned int   m_varsize;
+  unsigned int   m_offset;
+  EBIndex<cent>  m_index;
 };
 
 template <CENTERING cent, typename T>
@@ -39,17 +40,18 @@ getUglyStruct(const vector<EBIndex<cent> >& a_indices,
 }
 
 template <CENTERING cent, typename  data_t, unsigned int ncomp>
-inline vector< uglyStruct<data_t, ncomp> >
+inline vector< uglyStruct<cent, data_t, ncomp> >
 getUglyStruct(const vector<EBIndex<cent> >& a_indices,
               IrregData<cent, data_t, ncomp>& a_s )
 {
-  vector< uglyStruct<data_t, ncomp> > retval;
+  vector< uglyStruct<cent, data_t, ncomp> > retval;
   for(int ivec = 0; ivec < a_indices.size(); ivec++)
   {
-    uglyStruct<data_t, ncomp>  vecval;
+    uglyStruct<cent, data_t, ncomp>  vecval;
     vecval.m_startPtr = a_s.data();
     vecval.m_varsize  = a_s.vecsize();
-    vecval.m_index    = a_s.index(a_indices[ivec], 0);
+    vecval.m_offset   = a_s.index(a_indices[ivec], 0);
+    vecval.m_index    = a_indices[ivec];
     retval.push_back(vecval);
   }
   return retval;
@@ -59,7 +61,7 @@ getUglyStruct(const vector<EBIndex<cent> >& a_indices,
 #ifdef PROTO_CUDA
 template<typename Func, typename... Srcs>
 inline void
-cudaEBforAll(const Func & a_F,  Box a_box, Srcs&... a_srcs)
+cudaEBforall(const Func & a_F,  Box a_box, Srcs&... a_srcs)
 {
 }
 #else
@@ -71,27 +73,41 @@ getVar(unsigned int ivec,  T a_s)
   return a_s;
 }
 ///
-template<typename data_t, unsigned int ncomp>
+template<CENTERING cent, typename data_t, unsigned int ncomp>
 inline Var<data_t, ncomp>
 getVar(unsigned int a_ivec,
-       vector< uglyStruct<data_t, ncomp> > a_dst)
+       vector< uglyStruct<cent, data_t, ncomp> > a_dst)
 {
   Var<data_t, ncomp> retval;
-  const uglyStruct<data_t, ncomp> ugly = a_dst[a_ivec];
+  const uglyStruct<cent, data_t, ncomp> ugly = a_dst[a_ivec];
   for(int icomp = 0; icomp < ncomp; icomp++)
   {
-    retval.m_ptrs[icomp] = ugly.m_startPtr + ugly.m_index + (ugly.m_varsize*icomp);
+    retval.m_ptrs[icomp] = ugly.m_startPtr + ugly.m_offset + (ugly.m_varsize*icomp);
   }
   return retval;
 }
 ///going into this srcs are vector<uglystruct> and other stuff
-template<typename data_t,unsigned int ncomp,  typename Func, typename... Srcs>
+template<CENTERING cent, typename data_t,unsigned int ncomp,  typename Func, typename... Srcs>
 void
-vectorFunc(const Func& a_F, vector< uglyStruct<data_t, ncomp> > a_dst, Srcs... a_srcs)
+vectorFunc(const Func& a_F, vector< uglyStruct<cent, data_t, ncomp> > a_dst, Srcs... a_srcs)
 {
   for(unsigned int ivec = 0; ivec < a_dst.size(); ivec++)
   {
     a_F(getVar(ivec, a_dst), (getVar(ivec, a_srcs))...);
+  }
+       
+}
+
+
+///going into this srcs are vector<uglystruct> and other stuff
+template<CENTERING cent, typename data_t,unsigned int ncomp,  typename Func, typename... Srcs>
+void
+vectorFunc_p(const Func& a_F, vector< uglyStruct<cent, data_t, ncomp> > a_dst, Srcs... a_srcs)
+{
+  for(unsigned int ivec = 0; ivec < a_dst.size(); ivec++)
+  {
+    Point pt = a_dst[ivec].m_index.m_pt;
+    a_F(pt, getVar(ivec, a_dst), (getVar(ivec, a_srcs))...);
   }
        
 }
@@ -106,6 +122,19 @@ hostEBForAllIrreg(const Func& a_F, const Box& a_box,
   //indicies into irreg vector that correspond to input box
   vector<EBIndex<cent> > dstvofs = a_dst.getIndices(a_box);
   vectorFunc(a_F, getUglyStruct(dstvofs, a_dst), (getUglyStruct(dstvofs, a_srcs))...);
+}
+
+
+///going into this srcs are IrregDatas and other stuff
+template<CENTERING cent, typename  data_t, unsigned int ncomp, typename Func, typename... Srcs>
+inline void
+hostEBForAllIrreg_p(const Func& a_F, const Box& a_box,
+                    IrregData<cent, data_t, ncomp>& a_dst,
+                    Srcs&...  a_srcs)
+{
+  //indicies into irreg vector that correspond to input box
+  vector<EBIndex<cent> > dstvofs = a_dst.getIndices(a_box);
+  vectorFunc_p(a_F, getUglyStruct(dstvofs, a_dst), (getUglyStruct(dstvofs, a_srcs))...);
 }
 
 template<typename T>
@@ -125,18 +154,30 @@ getBoxData(EBBoxData<cent, data_t, ncomp>& a_s)
 ///going into this srcs are EBBoxDatas and other stuff
 template<typename Func, typename... Srcs>
 inline void
-hostEBforAll(const Func & a_F,  Box a_box, Srcs&... a_srcs)
+hostEBforall(const Func & a_F,  Box a_box, Srcs&... a_srcs)
 {
-  //call regular forall
+//call regular forall
   forallInPlaceBase(a_F, a_box, (getBoxData(a_srcs))...);
   
-  //do the same thing for the irregular data
+//do the same thing for the irregular data
   hostEBForAllIrreg(a_F, a_box, getIrregData(a_srcs)...);
+}
+
+///going into this srcs are EBBoxDatas and other stuff
+template<typename Func, typename... Srcs>
+inline void
+hostEBforall_p(const Func & a_F,  Box a_box, Srcs&... a_srcs)
+{
+//call regular forall
+  forallInPlaceBase_p(a_F, a_box, (getBoxData(a_srcs))...);
+  
+//do the same thing for the irregular data
+  hostEBForAllIrreg_p(a_F, a_box, getIrregData(a_srcs)...);
 }
 #endif
 
 template<typename Func, typename... Srcs>
-inline void EBforAllInPlace(unsigned long long int a_num_flops_point,
+inline void EBforallInPlace(unsigned long long int a_num_flops_point,
                             const char*            a_timername,
                             const Func & a_F,  Box a_box, Srcs&... a_srcs)
 {
@@ -146,9 +187,28 @@ inline void EBforAllInPlace(unsigned long long int a_num_flops_point,
   unsigned long long int boxfloops = a_num_flops_point*a_box.size();
 
 #ifdef PROTO_CUDA
-  cudaEBforAll(a_F, a_box, a_srcs...);
+  cudaEBforall(a_F, a_box, a_srcs...);
 #else
-  hostEBforAll(a_F, a_box, a_srcs...);
+  hostEBforall(a_F, a_box, a_srcs...);
+#endif
+  PR_FLOPS(boxfloops);
+}
+
+
+template<typename Func, typename... Srcs>
+inline void EBforallInPlace_p(unsigned long long int a_num_flops_point,
+                              const char*            a_timername,
+                              const Func & a_F,  Box a_box, Srcs&... a_srcs)
+{
+  PR_TIME(a_timername);
+
+
+  unsigned long long int boxfloops = a_num_flops_point*a_box.size();
+
+#ifdef PROTO_CUDA
+  cudaEBforall_p(a_F, a_box, a_srcs...);
+#else
+  hostEBforall_p(a_F, a_box, a_srcs...);
 #endif
   PR_FLOPS(boxfloops);
 }
@@ -168,6 +228,17 @@ PROTO_KERNEL_END(setUF, setU)
 
 
 PROTO_KERNEL_START 
+unsigned int setUptF(Point a_p, V a_U, double  a_val)
+{
+  for(int idir = 0; idir < DIM; idir++)
+  {
+    a_U(idir) = a_val;
+  }
+}
+PROTO_KERNEL_END(setUptF, setUpt)
+
+
+PROTO_KERNEL_START 
 unsigned int setVF(V a_V, double  a_val)
 {
   for(int idir = 0; idir < DIM; idir++)
@@ -176,6 +247,16 @@ unsigned int setVF(V a_V, double  a_val)
   }
 }
 PROTO_KERNEL_END(setVF, setV)
+
+PROTO_KERNEL_START 
+unsigned int setVptF(Point a_p, V a_V, double  a_val)
+{
+  for(int idir = 0; idir < DIM; idir++)
+  {
+    a_V(idir) = a_val;
+  }
+}
+PROTO_KERNEL_END(setVptF, setVpt)
 
 
 PROTO_KERNEL_START 
@@ -194,6 +275,24 @@ unsigned int setWtoUplusVF(V a_W,
   }
 }
 PROTO_KERNEL_END(setWtoUplusVF, setWtoUplusV)
+
+PROTO_KERNEL_START 
+unsigned int setWtoUplusVptF(Point a_pt,
+                             V a_W,
+                             V a_U,
+                             V a_V,
+                             double  a_val)
+{
+  for(int idir = 0; idir < DIM; idir++)
+  {
+    a_W(idir) = a_U(idir) + a_V(idir);
+    if(a_W(idir) != a_val)
+    {
+      printf("values do not match");
+    }
+  }
+}
+PROTO_KERNEL_END(setWtoUplusVptF, setWtoUplusVpt)
 
 int main(int argc, char* argv[])
 {
@@ -228,9 +327,16 @@ int main(int argc, char* argv[])
     double uval = 1;
     double vval = 2;
     double wval = 3;
-    EBforAllInPlace(numFlopsPt, "setU", setU, grid, U, uval);
-    EBforAllInPlace(numFlopsPt, "setV", setV, grid, V, vval);
-    EBforAllInPlace(numFlopsPt, "setWtoUPlusV", setWtoUplusV, grid, W, U, V, wval);
+    EBforallInPlace(numFlopsPt, "setU", setU, grid, U, uval);
+    EBforallInPlace(numFlopsPt, "setV", setV, grid, V, vval);
+    EBforallInPlace(numFlopsPt, "setWtoUPlusV", setWtoUplusV, grid, W, U, V, wval);
+
+    uval = 2;
+    vval = 5;
+    wval = 7;
+    EBforallInPlace_p(numFlopsPt, "setU", setUpt, grid, U, uval);
+    EBforallInPlace_p(numFlopsPt, "setV", setVpt, grid, V, vval);
+    EBforallInPlace_p(numFlopsPt, "setWtoUPlusV", setWtoUplusVpt, grid, W, U, V, wval);
 
 
   }
