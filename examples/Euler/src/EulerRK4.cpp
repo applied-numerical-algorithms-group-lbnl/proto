683b8bc4db405c3a63af36ff7cfbf366311de2b9
Phillip Colella <colella@phils-2790.local>
2020-03-28 23:05:33 -0700

    Changes to obtain correct convergence tests.
    Former-commit-id: a68a2f0420864717c0886838df363abf132718ce

--- examples/Euler/src/EulerRK4.cpp
+++ examples/Euler/src/EulerRK4.cpp
@@ -2,33 +2,44 @@
 
 int EulerRK4Op::s_count = 0;
 
-void outToFile(const BoxData<double,NUMCOMPS>& a_U,const char* a_str);
-
 EulerState::EulerState(const Box& a_box)
 {
     m_dbx0 = a_box;
-    m_dbx0.print();
-    m_U.define(m_dbx0.grow(NGHOST));
+    Box dbx = m_dbx0.grow(NGHOST);
+    m_U.define(m_dbx0);
+    Point ptValid = Point::Zeros();
+    // Setup for periodic BCs. Good for single Box data only.
+    for (int dir = 0; dir < DIM; dir++)
+      {
+       int size = m_dbx0.size(dir);
+       Box bValid = m_dbx0.grow(ptValid*NGHOST);
+       m_shift[2*dir]=Point::Basis(dir)*size;
+       m_bdry[2*dir] = bValid&dbx.shift(-m_shift[2*dir]);
+       m_shift[2*dir+1] = Point::Basis(dir)*(-size);
+       m_bdry[2*dir+1] = bValid&dbx.shift(-m_shift[2*dir+1]);
+       ptValid = ptValid + Point::Basis(dir);
+      }
 };
-
 void 
 EulerState::increment(const EulerDX& a_DX)
-{m_U += a_DX.m_DU;};
-
+{
+  m_U += a_DX.m_DU;
+};
 void 
 EulerDX::increment(double& a_weight,const EulerDX& a_incr)
 {
   BoxData<double,NUMCOMPS> temp(a_incr.m_DU.box());
-    (a_incr.m_DU).copyTo(temp);
-    temp *= a_weight;
-    m_DU += temp; 
+  (a_incr.m_DU).copyTo(temp);
+  temp *= a_weight;
+  m_DU += temp;
 };
 
 void 
-EulerDX::init(const EulerState& a_State)
+EulerDX::init(EulerState& a_State)
 {
     m_DU.define(a_State.m_dbx0);
     m_DU.setVal(0.);
+    m_box = a_State.m_dbx0;
 };
 
 void 
@@ -45,41 +56,24 @@ EulerRK4Op::operator()(
     Box dbx0 = a_State.m_dbx0;
     Box domain = dbx0;
     Box dbx = dbx0.grow(NGHOST);
-    BoxData<double,NUMCOMPS> U_ave(dbx);
-    a_State.m_U.copyTo(U_ave);
+    BoxData<double,NUMCOMPS> U_ave(dbx); 
+    (a_State.m_U).copyTo(U_ave);
+    
+     
     U_ave += a_DX.m_DU;
-
-//    int numghost = NGHOST;
-//    int numcomps = NUMCOMPS;
-//    for(int idir = 0; idir < DIM; idir++)
-//    {
-//      Point dirlo = -Point::Basis(idir);
-//      Point dirhi =  Point::Basis(idir);
-//      Box edgebxlo = domain.edge(dirlo);
-//      Box edgebxhi = domain.edge(dirhi);
-//
-//      Box dstbxlo = edgebxlo.extrude(idir,-(numghost-1));
-//      Box dstbxhi = edgebxhi.extrude(idir, (numghost-1));
-//
-//      //these are swapped because you have to move in different 
-//      //directions to get the periodic image
-//      Point shifthi = dirlo*domain.size(idir);
-//      Point shiftlo = dirhi*domain.size(idir);
-//
-//      Box srcbxlo = dstbxlo.shift(shiftlo);
-//      Box srcbxhi = dstbxhi.shift(shifthi);
-//
-//      U_ave.copy(U_ave, srcbxlo, 0, dstbxlo, 0, numcomps);
-//      U_ave.copy(U_ave, srcbxhi, 0, dstbxhi, 0, numcomps);
-//
-//    }
-
-
+    int numghost = NGHOST;
+    int numcomps = NUMCOMPS;
+    for(int dir = 0; dir < DIM; dir++)
+      {
+        //std::cout << 2*dir << " , " << a_State.m_bdry[2*dir] << " , "<< a_State.m_shift[2*dir] << std::endl;
+        //std::cout << U_ave.box() << " , " << U_ave.box().shift(a_State.m_shift[0]) << std::endl;
+        U_ave.copyTo(U_ave,a_State.m_bdry[2*dir], a_State.m_shift[2*dir]);
+        //std::cout << 2*dir+1 << std::endl;
+        U_ave.copyTo(U_ave,a_State.m_bdry[2*dir+1], a_State.m_shift[2*dir+1]);        
+      }
     Reduction<double>& rxn = a_State.m_Rxn;
     rxn.reset();
     EulerOp::step(a_DX.m_DU,U_ave,a_State.m_dbx0, rxn, false, true);
-    a_State.m_velSave = std::max(a_State.m_velSave, rxn.fetch());
-    a_DX.m_DU *= a_dt;
-  
+    a_State.m_velSave = std::max(a_State.m_velSave, rxn.fetch());  
     s_count += 1;
 };
