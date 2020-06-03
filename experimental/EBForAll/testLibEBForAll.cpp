139b63a06ffd67b987775b3a944929deb2bfb264
dtgraves <dtgraves@lbl.gov>
2020-06-03 14:59:43 -0700

    corrected test to fit new syntax
    Former-commit-id: 66ad410a2d0a1c19c13ba13938db35b1034d0252

--- experimental/EBForAll/testLibEBForAll.cpp
+++ experimental/EBForAll/testLibEBForAll.cpp
@@ -186,11 +186,12 @@ int main(int argc, char* argv[])
     vval = 5;
     wval = 7;
     printf("going into setUpt (fast version)\n");
-    ebFastforallInPlace_i(numFlopsPt, "setU", setUpt, grid, U, uval);
+    Box inputbox = U.inputBox();
+    ebFastforallInPlace_i(inputbox, numFlopsPt, "setU", setUpt, grid, U, uval);
     printf("going into setVpt(fast version)\n");
-    ebFastforallInPlace_i(numFlopsPt, "setV", setVpt, grid, V, vval, vvar);
+    ebFastforallInPlace_i(inputbox, numFlopsPt, "setV", setVpt, grid, V, vval, vvar);
     printf("going into setWpt(fast version)\n");
-    ebFastforallInPlace_i(numFlopsPt, "setWtoUPlusV", setWtoUplusVpt, grid, W, U, V, wval);
+    ebFastforallInPlace_i(inputbox, numFlopsPt, "setWtoUPlusV", setWtoUplusVpt, grid, W, U, V, wval);
     
 
   }
