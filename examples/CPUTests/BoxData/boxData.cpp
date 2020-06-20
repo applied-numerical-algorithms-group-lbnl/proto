29ec23911147224bf480c1f45e16e2639e7eea0d
Tuowen Zhao <ztuowen@gmail.com>
2020-06-20 13:07:31 -0600

    Added forall_p test
    Former-commit-id: 2f6be2c34b41d03bc5873cac7f1653a189358535

--- examples/CPUTests/BoxData/boxData.cpp
+++ examples/CPUTests/BoxData/boxData.cpp
@@ -100,18 +100,18 @@ TEST_F(ForallTest, forall) {
     auto last = boxData.var(p);
     for (int i = 0; i < DIM; ++i) {
       EXPECT_EQ(first(i), i + 1);
-      EXPECT_EQ(first(i), mid(i));
+      EXPECT_EQ(mid(i), first(i));
       EXPECT_EQ(midp(i), 0);
       EXPECT_EQ(last(i), 0);
     }
   }
   // Partial set out-of-place
   // This uses stack allocator
-  auto addMagic = [](Var<ValType, DIM> &dst, Var<ValType, DIM> &src) {
+  auto mulMagic = [](Var<ValType, DIM> &dst, Var<ValType, DIM> &src) {
     for (int i = 0; i < DIM; ++i)
       dst(i) = (i + 1) * src(i);
   };
-  auto forall_dst = forall<ValType, DIM>(addMagic, Box(p_partial), boxData);
+  auto forall_dst = forall<ValType, DIM>(mulMagic, Box(p_partial), boxData);
   {
     EXPECT_EQ(forall_dst.box(), Box(p_partial));
     EXPECT_EQ(forall_dst.box(), forall_dst.padded_box());
@@ -121,9 +121,89 @@ TEST_F(ForallTest, forall) {
     auto orig_last = boxData.var(p);
     for (int i = 0; i < DIM; ++i) {
       EXPECT_EQ(first(i), (i + 1) * (i + 1));
-      EXPECT_EQ(first(i), last(i));
+      EXPECT_EQ(last(i), first(i));
       EXPECT_EQ(orig_first(i), i + 1);
       EXPECT_EQ(orig_last(i), 0);
+    }
+  }
+  // Intersect
+  Box intersect = Box(p_partial).shift(Point::Ones());
+  forallInPlace(setMagic, intersect, forall_dst);
+  {
+    auto first = forall_dst.var(Point::Zeros());
+    auto mid = forall_dst.var(Point::Ones());
+    auto last = forall_dst.var(p_partial);
+    for (int i = 0; i < DIM; ++i) {
+      EXPECT_EQ(first(i), (i + 1) * (i + 1));
+      EXPECT_EQ(mid(i), i + 1);
+      EXPECT_EQ(last(i), mid(i));
+    }
+  }
+  // Empty box
+  forallInPlace(setMagic, Box(), forall_dst);
+  {
+    auto first = forall_dst.var(Point::Zeros());
+    auto last = forall_dst.var(p_partial);
+    for (int i = 0; i < DIM; ++i) {
+      EXPECT_EQ(first(i), i + 1);
+      EXPECT_EQ(last(i), first(i));
+    }
+  }
+}
+
+TEST_F(ForallTest, forall_p) {
+  auto setMagic = [](const Point &p, Var<ValType, DIM> &var) {
+    for (int i = 0; i < DIM; ++i)
+      var(i) = p[i];
+  };
+  // Partial set
+  Point p_partial = Point(15, 31, 15, 31, 63);
+  Point p_arb = Point(1, 2, 3, 4, 5, 6);
+  forallInPlace_p(setMagic, Box(p_partial), boxData);
+  {
+    auto first = boxData.var(p_arb);
+    auto mid = boxData.var(p_partial);
+    auto midp = boxData.var(p_partial + Point::Ones());
+    auto last = boxData.var(p);
+    for (int i = 0; i < DIM; ++i) {
+      EXPECT_EQ(first(i), i + 1);
+      EXPECT_EQ(mid(i), p_partial[i]);
+      EXPECT_EQ(midp(i), 0);
+      EXPECT_EQ(last(i), 0);
+    }
+  }
+  // Partial set out-of-place
+  // This uses stack allocator
+  auto mulMagic = [](const Point &p, Var<ValType, DIM> &dst, Var<ValType, DIM> &src) {
+    for (int i = 0; i < DIM; ++i)
+      dst(i) = p[i] * src(i);
+  };
+  auto forall_dst = forall_p<ValType, DIM>(mulMagic, Box(p_partial), boxData);
+  {
+    EXPECT_EQ(forall_dst.box(), Box(p_partial));
+    EXPECT_EQ(forall_dst.box(), forall_dst.padded_box());
+    auto first = forall_dst.var(p_arb);
+    auto last = forall_dst.var(p_partial);
+    auto orig_first = boxData.var(p_arb);
+    auto orig_last = boxData.var(p);
+    for (int i = 0; i < DIM; ++i) {
+      EXPECT_EQ(first(i), (i + 1) * (i + 1));
+      EXPECT_EQ(last(i), p_partial[i] * p_partial[i]);
+      EXPECT_EQ(orig_first(i), i + 1);
+      EXPECT_EQ(orig_last(i), 0);
+    }
+  }
+  // Intersect
+  Box intersect = Box(p_partial).shift(Point::Ones());
+  forallInPlace_p(setMagic, intersect, forall_dst);
+  {
+    auto first = forall_dst.var(p_arb - Point::Ones());
+    auto mid = forall_dst.var(p_arb + Point::Ones());
+    auto last = forall_dst.var(p_partial);
+    for (int i = 0; i < DIM; ++i) {
+      EXPECT_EQ(first(i), i * i);
+      EXPECT_EQ(mid(i), i + 2);
+      EXPECT_EQ(last(i), p_partial[i]);
     }
   }
 }
