17d57312193bfa556b4b6bfd7af9b2c61e1ab3f1
Phillip Colella <colella@phils-2790.local>
2020-08-11 21:51:11 -0700

    As of these changes, new levelBoxData now compiles and executes to completion.
    Former-commit-id: 8d3f66358b744ac01ba4d0f5f615e6dd113bf0bf

--- examples/MultigridLevelData/exec/multigridTest.cpp
+++ examples/MultigridLevelData/exec/multigridTest.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <fstream>
 #include "Proto.H"
+#include "Proto_SPMD.H"
 #include <iostream>
 #include <cstring>
 #include <memory>
@@ -24,31 +25,55 @@ void rhsPoint(const Point& a_pt,Var<double> a_rho,double a_h)
 }
 int main(int argc, char* argv[])
 {
+#ifdef PR_MPI
+  MPI_Init (&argc, &argv);
+#endif
   int logDomainSize;
   int numLevels;
   int maxiter;
   double tol;
-  cout << "input log_2(domainSize), number of multigrid levels" << endl;
-  cin >> logDomainSize >> numLevels;
-  cout << "input max number of iterations, convergence tolerance " << endl;
-  cin >> maxiter >> tol;
-  int domainSize = ipow(2,logDomainSize);
-  PR_TIMER_SETFILE(to_string(domainSize) + "proto.time.table");
-  PR_TIMERS("main");
-  
-  Box domain(Point::Zeros(),Point::Ones()*(domainSize -1));
-  array<bool,DIM> per;
-  for(int idir = 0; idir < DIM; idir++) per[idir]=true;
-  double dx = 1.0/domainSize;
-  DisjointBoxLayout bl(domain,min(MAXBOXSIZE,domainSize),per);
+  int myproc = procID();
+#ifdef PR_MPI
+      if (myproc == 0)
+#endif
+        {
+      cout << "input log_2(domainSize), number of multigrid levels" << endl;
+      cin >> logDomainSize >> numLevels; 
+      cout << "input max number of iterations, convergence tolerance " << endl;
+      cin >> maxiter >> tol;
+        }
+#ifdef PR_MPI
+      MPI_Bcast(&logDomainSize, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+      MPI_Bcast(&numLevels, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+      MPI_Bcast(&maxiter, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+      MPI_Bcast(&tol, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+#endif
+      barrier();
+      cout << "numLevels = " << numLevels << ", myproc = " << myproc << endl;
+      int domainSize = ipow(2,logDomainSize);
+      
+ PR_TIMER_SETFILE(to_string(domainSize) + "proto.time.table");
+ PR_TIMERS("main");
+
+ Box domain(Point::Zeros(),Point::Ones()*(domainSize -1));
+ array<bool,DIM> per;
+ for(int idir = 0; idir < DIM; idir++) per[idir]=true;
+ double dx = 1.0/domainSize;
+ int scalarBoxSize = 32;
+ int modulus = domainSize % scalarBoxSize;
+ PROTO_ASSERT((modulus == 0),"Domain not nested");
+ ProblemDomain pd(domain,per);
+ DisjointBoxLayout bl(pd,scalarBoxSize*Point::Ones());
 
   LevelBoxData<double > rho(bl,Point::Zeros());
   LevelBoxData<double > phi(bl,Point::Ones());
+#if 0  
   rho.setToZero();
   phi.setToZero();
-  for (int i = 0; i < bl.size();i++)
+
+  for (auto dit = phi.begin();*dit != dit.end();++dit)
     {
-      BoxData<double>& rhoPatch = rho[i];
+      BoxData<double>& rhoPatch = rho[*dit];
       forallInPlace_p(rhsPoint,rhoPatch,dx);
     }
   LevelMultigrid mg(domain,dx,numLevels);
@@ -65,5 +90,9 @@ int main(int argc, char* argv[])
         if (resnorm < tol*resnorm0) break;
       }
   }
+#endif
   PR_TIMER_REPORT();
+#ifdef PR_MPI
+  MPI_Finalize();
+#endif
 }
