87bd804bf81b2b04de693b06d98cab460e6417b6
Dharshi Devendran <pdevendran@lbl.gov>
2020-11-17 16:32:37 -0800

    Update absMax calls in Euler examples to use reduction
    Former-commit-id: 7865f779949dc4478a3c6e37815d772899ab29bf

--- examples/Euler/exec/Euler.cpp
+++ examples/Euler/exec/Euler.cpp
@@ -190,15 +190,18 @@ int main(int argc, char* argv[])
     }
   if (convTest)
     {
+        Reduction<double> rxn;
       double rhoErrMax[2];
       for (int lev = 0; lev < maxLev-1; lev++)
         {
+            rxn.reset();
           err[lev] |= Stencil<double>::AvgDown(2)(U[lev+1]);
           err[lev] -= U[lev];
           int size = U[lev].box().size(0);
           auto errRho = slice(err[lev],0);
           auto rhoLev = slice(U[lev],0);
-          rhoErrMax[lev] = errRho.absMax();
+          errRho.absMax(rxn);
+          rhoErrMax[lev] = rxn.fetch();
           string errStr = "error_"+std::to_string(size);
           string rhoStr = "rho_"+std::to_string(size);
           WriteBoxData(errStr.c_str(),errRho,1./size);
