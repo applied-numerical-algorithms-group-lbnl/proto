967b860f78e846f1bc1755ac57d64ad53aaf0723
bvs <bvstraalen@lbl.gov>
2021-02-17 15:28:05 -0500

    recovered PROTO_LAMBDA
    Former-commit-id: 7628935db02ac50efe812b30eb184cb4a1b707f9

--- examples/levelRelax/exec/main.cpp
+++ examples/levelRelax/exec/main.cpp
@@ -132,8 +132,8 @@ int main(int argc, char* argv[])
             auto& rhs = RHS[*dit];
             BoxData<double> temp = negoperator(u);
           // forallInPlaceOp(PFLOPS, "algebra parts", fusedRelax, u, temp, rhs, lambda);
-           forallInPlaceOp(PFLOPS, "algebra parts", PROTO_LAMBDA [=](Var<double> u, Var<double> temp, Var<double> rhs)
-                            {    u(0) += lambda*(rhs(0)-temp(0));}, u, temp, rhs);	
+           forallInPlaceOp(PFLOPS, "algebra parts", [] PROTO_LAMBDA (Var<double> u, Var<double> temp, Var<double> rhs, double lambda)
+                            {    u(0) += lambda*(rhs(0)-temp(0));}, u, temp, rhs, lambda);	
             
           }
       }
