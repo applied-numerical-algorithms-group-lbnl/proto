2d3172372b3651d5c03fc044f9cabc72fa6fde78
cgebhart <cgebhart@lbl.gov>
2021-08-12 09:39:42 -0700

    Checked out some updated files from master
    Former-commit-id: df9ea3ae071101781713ad3420e8a6d2e50916d6

---
+++ examples/HDF5/exec/testH5.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "Proto.H"
+
+#define D 3
+
+using namespace Proto;
+
+void f_init(Point& a_pt, Var<double, D>& a_data, array<double, DIM> a_dx)
+{
+   
+    double x[DIM];
+   
+    for (int ii = 0; ii < DIM; ii++)
+    {
+        x[ii] = a_pt[ii]*a_dx[ii] + a_dx[ii]/2.0;
+    }
+    
+    a_data(0) = sin(2.0*M_PI*(x[0] + x[1]));
+    a_data(1) = sin(2.0*M_PI*(x[0] - x[1]));
+    a_data(2) = a_data(0) + a_data(1);
+}
+
+int main(int argc, char** argv)
+{
+    PR_TIMER_SETFILE("testH5.timer");
+    PR_TIME("main");
+    #ifdef PR_MPI
+    MPI_Init(&argc, &argv);
+    #endif
+    
+    double L = 1.0;
+    int domainSize = 64;
+    if (argc > 1 )
+    {
+        domainSize = atoi(argv[1]);    
+    }
+
+    Point boxSize = Point::Ones(domainSize / 8);
+    Box domainBox = Box::Cube(domainSize);
+    array<bool, DIM> periodicity;
+    for (int ii = 0; ii < DIM; ii++){ periodicity[ii] = true; }
+    ProblemDomain domain(domainBox, periodicity);
+    DisjointBoxLayout layout(domain, boxSize);
+    
+    LevelBoxData<double, D> writtenData(layout, Proto::Point::Ones(2)); 
+    LevelBoxData<double, D> readData(layout, Proto::Point::Ones(2)); 
+    array<double, DIM> dx;
+    for (int ii = 0; ii < DIM; ii++){ dx[ii] = L/domainSize; }
+
+    for (auto iter = writtenData.begin(); *iter != iter.end(); ++iter)
+    {
+        auto& patch = writtenData[*iter];
+        forallInPlace_p(f_init, patch, dx);
+    }
+
+    HDF5Handler h5;
+    auto iter = writtenData.begin();
+    auto& patch = writtenData[*iter];
+    h5.writePatch(patch, {"alpha", "beta", "gamma"}, dx, "PatchData");
+    h5.writeLevel(writtenData, {"alpha", "beta", "gamma"}, dx,"LevelData");
+    barrier();
+    h5.readLevel(readData, "LevelData");
+    double error = 0.0;
+    for (auto iter = writtenData.begin(); *iter != iter.end(); ++iter)
+    {
+        auto& wpatch = writtenData[*iter];
+        auto& rpatch = readData[*iter];
+        wpatch -= rpatch;
+        error = max(error, wpatch.absMax());
+    }
+    std::cout << "Write -> Read Error: " << error << std::endl;
+    #ifdef PR_MPI
+    MPI_Finalize();
+    #endif
+    PR_TIMER_REPORT();
+}
