05bc49a64e6a99420dcd4b9a5bd7a4c74bc1e0d0
cgebhart <cgebhart@lbl.gov>
2021-08-23 15:46:49 -0700

    Modifying the testLevelData example to test irregular data layouts
    Former-commit-id: bc17497b3a87f6ee474787ab13b70cd7221b6195

--- examples/testLevelData/exec/testLevelData.cpp
+++ examples/testLevelData/exec/testLevelData.cpp
@@ -7,67 +7,36 @@ int main(int argc, char** argv)
 #ifdef PR_MPI
     MPI_Init(&argc, &argv);
 #endif
-    int domainSize = 16;
+    int domainSize;
+    int boxSize;
+    if (procID() == 0)
+    {
+        if (argc > 1)
+        {
+            domainSize = atoi(argv[1]);
+        }
+        if (argc > 2)
+        {
+            boxSize = atoi(argv[2]);
+        }
+    }
+
+#ifdef PR_MPI
+    MPI_Bcast(&domainSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&boxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+#endif
+
     Box domain = Box::Cube(domainSize);
-    Point boxSize = Point::Ones(4);
+    Point boxSizeVect = Point::Ones(boxSize);
+
     Point ghost = Point::Ones();
     std::array<bool, DIM> periodicity;
     for (int ii = 0; ii < DIM; ii++) { periodicity[ii] = true; }
     
     ProblemDomain problemDomain(domain, periodicity);
-    DisjointBoxLayout layout(problemDomain, boxSize);
+    DisjointBoxLayout layout(problemDomain, boxSizeVect);
 
-    /*
-    for (auto iter = layout.begin(); iter.ok(); ++iter)
-    {
-        if (procID() == 0)
-        {
-            auto b0 = layout[*iter];
-            auto b =  layout.domain() & b0.grow(ghost);
-            std::cout << "Source Box = " << b0 << std::endl;
-            std::cout << "Neighbors: " << std::endl;
-            NeighborIterator niter(layout, b);
-            for (niter.begin(); *niter != niter.end(); ++niter)
-            {
-                std::cout << "src box: " << niter.srcBox() << " | dst box: " << niter.destBox() << " | shift: " << niter.shift() << " | proc: " << niter.procID() << std::endl;
-            }
-        }
-    }
-    */
-    LevelBoxData<double> src(layout, ghost);
-    //LevelBoxData<double> dst(layout, ghost);
 
-    for (auto iter = layout.begin(); iter.ok(); ++iter)
-    {
-        src[*iter].setVal((*iter)+1, layout[*iter]);
-        //dst[*iter].setVal(0);
-    }
-
-    HDF5Handler h5;
-
-    h5.writeLevel(src, "SRC_0.hdf5");
-    //h5.writeLevel(dst, "DST_0.hdf5");
-
-    src.exchange();
-    //src.copyTo(dst);
-    
-    h5.writeLevel(src, "SRC_1.hdf5");
-    //h5.writeLevel(dst, "DST_1.hdf5");
-
-    LevelBoxData<double> error(layout, ghost);
-    LevelBoxData<double> soln(layout, ghost);
-
-    h5.readLevel(error, "saved_data/new.hdf5");
-    h5.readLevel(soln, "saved_data/master.hdf5");
-
-    for (auto iter = error.begin(); iter.ok(); ++iter)
-    {
-        auto& err = error[*iter];
-        auto& sln = soln[*iter];
-        err -= sln;
-    }
-
-    h5.writeLevel(error, "error.hdf5");
 
 #ifdef PR_MPI
     MPI_Finalize();
