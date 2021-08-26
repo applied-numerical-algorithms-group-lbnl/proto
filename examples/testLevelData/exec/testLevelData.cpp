b98cda4a8cd9b87d612d9e07368798b3175971c0
Chris Gebhart <cgebhart@lbl.gov>
2021-08-26 16:10:34 -0700

    minor changes to testLevelData example
    Former-commit-id: 42752918240459debad7d3ce4fd102d2a5aac937

--- examples/testLevelData/exec/testLevelData.cpp
+++ examples/testLevelData/exec/testLevelData.cpp
@@ -11,6 +11,7 @@ int main(int argc, char** argv)
     int boxSize;
     if (procID() == 0)
     {
+        std::cout << "usage: ./testLevelData domainSize boxSize" << std::endl;
         if (argc > 1)
         {
             domainSize = atoi(argv[1]);
@@ -19,12 +20,15 @@ int main(int argc, char** argv)
         {
             boxSize = atoi(argv[2]);
         }
+        std::cout << "domainSize = " << domainSize << " | boxSize = " << boxSize << std::endl;
     }
 
 #ifdef PR_MPI
     MPI_Bcast(&domainSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&boxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 #endif
+
+    HDF5Handler h5;
 
     Box domain = Box::Cube(domainSize);
     Point boxSizeVect = Point::Ones(boxSize);
@@ -35,9 +39,32 @@ int main(int argc, char** argv)
     
     ProblemDomain problemDomain(domain, periodicity);
     DisjointBoxLayout layout(problemDomain, boxSizeVect);
+    
+    LevelBoxData<double> src(layout, ghost);
+    LevelBoxData<double> dst(layout, ghost);
+    for (auto iter = src.begin(); iter.ok(); ++iter)
+    {
+        auto& patch = src[*iter];
+        patch.setVal(*iter, layout[*iter]);
+    }
 
+    h5.writeLevel(src, "SRC_0");
+    h5.writeLevel(dst, "DST_0");
+    src.exchange();
+    h5.writeLevel(src, "SRC_1");
+    src.copyTo(dst);
+    h5.writeLevel(dst, "DST_1");
 
-
+    bool success = true;
+    Reduction<double> rxn;
+    for (auto iter = layout.begin(); iter.ok(); ++iter)
+    {
+        auto& patch = src[*iter];
+        patch.absMax(rxn);
+        //double max = rxn.fetch();
+        double max = patch.absMax();
+        std::cout << "Proc: " << procID() << " | patch: " << *iter << " | abs-max: " << max << std::endl;
+    }
 #ifdef PR_MPI
     MPI_Finalize();
 #endif
