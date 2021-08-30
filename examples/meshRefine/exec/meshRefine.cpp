304021dcdf8f4f2478b877c61ec0a084b3f3a002
cgebhart <cgebhart@lbl.gov>
2021-08-30 12:14:09 -0700

    AMRGRid::buffer seems to be working correctly. Progress made on AMRGrid::regrid
    Former-commit-id: abea210d3e1bc7ce8f48601a5a002374152739ea

--- examples/meshRefine/exec/meshRefine.cpp
+++ examples/meshRefine/exec/meshRefine.cpp
@@ -2,12 +2,25 @@
 
 using namespace Proto;
 
-void foo (Point& a_pt, Var<double>& a_data, double a_dx)
+void f_sin (Point& a_pt, Var<double>& a_data, double a_dx)
 {
     double x = a_pt[0]*a_dx + a_dx/2.0;
     double y = a_pt[1]*a_dx + a_dx/2.0;
     
     a_data(0) = sin(2.0*M_PI*(x + y));
+}
+
+void f_tags (Point& a_pt, Var<char>& a_data, double a_dx, Point a_origin)
+{
+    double x = (a_pt[0]-a_origin[0])*a_dx + a_dx/2.0;
+    double y = (a_pt[1]-a_origin[0])*a_dx + a_dx/2.0;
+    
+    if (abs(x-y) < 3*a_dx)
+    {
+        a_data(0) = 1;
+    } else {
+        a_data(0) = 0;
+    }
 }
 
 int main(int argc, char** argv)
@@ -34,104 +47,37 @@ int main(int argc, char** argv)
     MPI_Bcast(&boxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 #endif
 
+    HDF5Handler h5;
+
     double L = 1.0;
     double k = 2.0*M_PI;
     int refRatio = 2;
 
-    for (int nn = 0; nn < 3; nn++)
+    for (int nn = 0; nn < 1; nn++)
     {
         double dx = L / domainSize;
-        double fdx = dx/2.0;
+        Point origin = Point::Ones(domainSize / 2);
 
-        int fineDomainSize = domainSize * refRatio;
-        Box crseDomain = Box::Cube(domainSize);
-        Box fineDomain = Box::Cube(fineDomainSize);
-        Box subDomain  = Box::Cube(fineDomainSize / 2);
-        //subDomain = subDomain.shift(Point::Ones(fineDomainSize / 4));
-        Box subDomainPatches = subDomain.coarsen(boxSize);
-        std::vector<Point> subDomainPatchVector;
-        for (auto biter = subDomainPatches.begin();
-                biter != subDomainPatches.end(); ++biter)
-        {
-            subDomainPatchVector.push_back(*biter);
-            subDomainPatchVector.push_back(*biter + subDomainPatches.high() + Point::Ones());
-        }
-        
+        Box domain = Box::Cube(domainSize);
         Point boxSizeVect = Point::Ones(boxSize);
-        Point ghost = Point::Ones();
-        std::array<bool, DIM> periodicity;
-        for (int ii = 0; ii < DIM; ii++) { periodicity[ii] = true; }
+        Point tagBufferSize = Point::Ones(2);
+        ProblemDomain problemDomain(domain, true);
+        DisjointBoxLayout layout(problemDomain, boxSizeVect);
+        
+        LevelTagData tags(layout, tagBufferSize);
 
-        ProblemDomain crseProblemDomain(crseDomain, periodicity);
-        ProblemDomain fineProblemDomain(fineDomain, periodicity);
+        tags.initialize(f_tags, dx, origin);
+        
+        h5.writeLevel(tags, "Tags_0"); 
 
-        DisjointBoxLayout crseLayout(crseProblemDomain, boxSizeVect);
-        DisjointBoxLayout fineLayout(fineProblemDomain, subDomainPatchVector, boxSizeVect);
-        DisjointBoxLayout crseFineLayout = fineLayout.coarsen(Point::Ones(refRatio));
+        AMRGrid::buffer(tags, 2);
+        
+        h5.writeLevel(tags, "Tags_1"); 
 
-        crseLayout.print("Coarse Layout");
-        fineLayout.print("Fine Layout"); 
-        crseFineLayout.print("Coarse Fine Layout");
+        AMRGrid grid;
 
-        auto laplace = Stencil<double>::Laplacian();
+        grid.regrid(tags, 0);
 
-        LevelBoxData<double> phi_c(crseLayout,  ghost);
-        LevelBoxData<double> phi_f(fineLayout,  ghost);
-        LevelBoxData<double> Lphi_c(crseLayout, ghost);
-        LevelBoxData<double> Lphi_f(fineLayout, ghost);
-        LevelBoxData<double> err_c(crseLayout,  Point::Zeros());
-        LevelBoxData<double> err_f(fineLayout,  Point::Zeros());
-
-        phi_c.initialize(foo, dx);
-        phi_f.initialize(foo, fdx);
-
-        for (auto iter = crseLayout.begin(); iter.ok(); ++iter)
-        {
-            auto& phi = phi_c[*iter];
-            auto& Lphi = Lphi_c[*iter];
-
-            Lphi |= laplace(phi, -1.0/(2.0*k*k*dx*dx));
-        }
-
-        for (auto iter = fineLayout.begin(); iter.ok(); ++iter)
-        {
-            auto& phi = phi_f[*iter];
-            auto& Lphi = Lphi_f[*iter];
-
-            Lphi |= laplace(phi, -1.0/(2.0*k*k*fdx*fdx));
-        }
-
-        HDF5Handler h5;
-        std::vector<std::string> vars_phi  = {"phi"};
-        std::vector<std::string> vars_err  = {"error"};
-        std::vector<std::string> vars_Lphi = {"Lphi"};
-
-        h5.writeLevel(Lphi_c, vars_Lphi, dx,   "Lphi_C_N%i", nn);
-        h5.writeLevel(Lphi_f, vars_Lphi, dx/2, "Lphi_F_N%i", nn);
-        h5.writeLevel(phi_c,  vars_phi,  dx,   "phi_C_N%i", nn);
-        h5.writeLevel(phi_f,  vars_phi,  dx/2, "phi_F_N%i", nn);
-
-        for (auto iter = crseLayout.begin(); iter.ok(); ++iter)
-        {
-            auto& err = err_c[*iter];
-            auto& phi = phi_c[*iter];
-            auto& Lphi = Lphi_c[*iter];
-            Lphi.copyTo(err);
-            err -= phi;
-        }
-
-        for (auto iter = fineLayout.begin(); iter.ok(); ++iter)
-        {
-            auto& err = err_f[*iter];
-            auto& phi = phi_f[*iter];
-            auto& Lphi = Lphi_f[*iter];
-            Lphi.copyTo(err);
-            err -= phi;
-        }
-
-        h5.writeLevel(err_c, vars_err, dx, "Error_C_N%i", nn);
-        h5.writeLevel(err_f, vars_err, dx/2, "Error_F_N%i", nn);
-    
         domainSize *= 2;
     }
 #ifdef PR_MPI
