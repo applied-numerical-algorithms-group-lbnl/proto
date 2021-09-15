446b96c9a0dae568674071ac4ef5ee128c9df580
cgebhart <cgebhart@lbl.gov>
2021-09-15 11:42:29 -0700

    Both AMRGrid::regrid and AMRGrid::enforceNesting appear correct in serial
    Former-commit-id: abc3029da8ec63e19512a80e4885ae1ef88cf2b6

--- examples/meshRefine/exec/meshRefine.cpp
+++ examples/meshRefine/exec/meshRefine.cpp
@@ -1,4 +1,5 @@
 #include "Proto.H"
+#include "InputParser.H"
 
 using namespace Proto;
 
@@ -66,28 +67,29 @@ int main(int argc, char** argv)
 #ifdef PR_MPI
     MPI_Init(&argc, &argv);
 #endif
-    int domainSize = 8;
-    int boxSize = 4;
-    int TEST_NUM = 0;
+
+    InputArgs args;
+    args.parse("inputs");
+     
+    int TEST_NUM = -1;
     if (procID() == 0)
     {
         if (argc > 1)
         {
-            domainSize = atoi(argv[1]);
+            TEST_NUM = atoi(argv[1]);
         }
-        if (argc > 2)
+        if (TEST_NUM == -1)
         {
-            boxSize = atoi(argv[2]);
-        }
-        if (argc > 3)
-        {
-            TEST_NUM = atoi(argv[3]);
+            std::cout << "Usage: ./meshRefine.exe testNum" << std::endl;
+            std::cout << "Tests:" << std::endl;
+            std::cout << "\tTest 0: Refine 2D Diagonal Line" << std::endl;
+            std::cout << "\tTest 1: Refine ND Hollow Sphere" << std::endl;
+            std::cout << "\tTest 10: Enforce Nesting In Bulk Domain" << std::endl;
+            std::cout << "\tTest 11: Enforce Nesting At Periodic Boundaries" << std::endl;
         }
     }
 
 #ifdef PR_MPI
-    MPI_Bcast(&domainSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&boxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&TEST_NUM, 1, MPI_INT, 0, MPI_COMM_WORLD);
 #endif
 
@@ -97,6 +99,8 @@ int main(int argc, char** argv)
     int bufferSize = 1;
     for (int nn = 0; nn < 1; nn++)
     {
+        int domainSize = args.get("domainSize");
+        int boxSize = args.get("boxSize");
         double dx = L / domainSize;
         Point origin = Point::Ones(domainSize / 2);
        
@@ -151,10 +155,12 @@ int main(int argc, char** argv)
             Point originPatch = origin / fineBoxSizeVect * PR_AMR_REFRATIO; 
             if (procID() == 0)
             {
-                std::cout << "Created mesh is radially symmetric: " << grid[1].radialSymmetry(originPatch) << std::endl;
+                std::cout << "Created mesh is radially symmetric: ";
+                std::cout << grid[1].radialSymmetry(originPatch) << std::endl;
                 for (int ii = 0; ii < DIM; ii++)
                 {
-                    std::cout << "Created mesh is symmetric in coordinate " << ii << ": " << grid[1].mirrorSymmetry(originPatch, ii) << std::endl;
+                    std::cout << "Created mesh is symmetric in coordinate " << ii;
+                    std::cout << ": " << grid[1].mirrorSymmetry(originPatch, ii) << std::endl;
                 }
             }
 
@@ -163,109 +169,208 @@ int main(int argc, char** argv)
 
             h5.writeAMRData({"data"}, 1.0, data, "Grids");
             break;
+            // ===========================================================
+            // TEST 1
+            } case 1: {
+            if (procID() == 0)
+            {
+                std::cout << "Running Test 1: ND Hollow Sphere" << std::endl;
+            }
+            Box domain = Box::Cube(domainSize);
+
+            Point tagBufferSize = Point::Ones(bufferSize);
+            Point boxSizeVect = Point::Ones(boxSize);
+            Point fineBoxSizeVect = Point::Ones(boxSize / 2); 
+            std::array<bool, DIM> periodicity;
+            for (int ii = 0; ii < DIM; ii++)
+            {
+                if (ii == 0) { periodicity[ii] = true; }
+                else {periodicity[ii] = false; }
+            }
             
+            if (procID() == 0)
+            {
+                std::cout << "\tTag Buffer Size: " << tagBufferSize << std::endl;
+                std::cout << "\tBox Size (Coarse): " << boxSizeVect << std::endl;
+                std::cout << "\tBox Size (Fine): " << fineBoxSizeVect << std::endl;
+            }
+
+            ProblemDomain problemDomain(domain, periodicity);
+            DisjointBoxLayout layout(problemDomain, boxSizeVect);
+            DisjointBoxLayout fineLayout(problemDomain.refine(Point::Ones(PR_AMR_REFRATIO)), fineBoxSizeVect);
+            std::vector<DisjointBoxLayout> layouts;
+            layouts.push_back(layout);
+            layouts.push_back(fineLayout);
+
+            AMRGrid grid(layouts, 2);
+            AMRData<double> testData(grid, Point::Zeros());
+            testData[0].initialize(f_const, 1, 0);
+            testData[1].initialize(f_const, 1, 1);
+            h5.writeAMRData({"data"}, 1.0, testData, "TestGrids");
+
+            LevelTagData tags(layout, tagBufferSize);
+            tags.initialize(f_tags_sphere, dx, origin);
+
+            h5.writeLevel(tags, "Tags_0"); 
+            AMRGrid::buffer(tags, tagBufferSize);
+            h5.writeLevel(tags, "Tags_1"); 
+
+            grid.regrid(tags, 0, fineBoxSizeVect);
+
+            Point originPatch = origin / fineBoxSizeVect * PR_AMR_REFRATIO; 
+            if (procID() == 0)
+            {
+                std::cout << "Created mesh is radially symmetric: ";
+                std::cout << grid[1].radialSymmetry(originPatch) << std::endl;
+                for (int ii = 0; ii < DIM; ii++)
+                {
+                    std::cout << "Created mesh is symmetric in coordinate " << ii;
+                    std::cout << ": " << grid[1].mirrorSymmetry(originPatch, ii) << std::endl;
+                }
+            }
+
+            AMRData<double> data(grid, Point::Zeros());
+            data.setToZero();
+
+            h5.writeAMRData({"data"}, 1.0, data, "Grids");
+            break;
+            // ===========================================================
+            // TEST 10
+            } case 10: {
+            
+            if (procID() == 0)
+            {
+                std::cout << "Running Test 10: Enforce Nesting In Bulk Domain" << std::endl;
+            }
+            int nestingDistance = args.get("nestingDistance");
+            double dx_vect[3];
+            dx_vect[0] = dx;
+            dx_vect[1] = dx_vect[0] / PR_AMR_REFRATIO;
+            dx_vect[2] = dx_vect[1] / PR_AMR_REFRATIO;
+
+            Box domain = Box::Cube(domainSize);
+
+            Point boxSizeVect = Point::Ones(boxSize);
+            Point fineBoxSizeVect = Point::Ones(boxSize / 2); 
+            std::array<bool, DIM> periodicity;
+            for (int ii = 0; ii < DIM; ii++)
+            {
+                if (ii == 0) { periodicity[ii] = true; }
+                else {periodicity[ii] = false; }
+            }
+            
+            if (procID() == 0)
+            {
+                std::cout << "\tBox Size (Coarse): " << boxSizeVect << std::endl;
+                std::cout << "\tBox Size (Fine): " << fineBoxSizeVect << std::endl;
+            }
+            ProblemDomain problemDomain(domain, periodicity);
+            DisjointBoxLayout layout(problemDomain, boxSizeVect);
+
+            AMRGrid grid(layout, 3);
+            
+            LevelTagData tags_0(layout, Point::Zeros());
+            Point corner_0 = Point::Ones(boxSize) + Point::Basis(0, 1);
+            tags_0.initialize(f_tags_corner, dx_vect[0], corner_0);
+            h5.writeLevel(dx_vect[0], tags_0, "Tags_0");
+
+            grid.regrid(tags_0, 0, boxSizeVect);
+
+            LevelTagData tags_1(grid[1], Point::Zeros());
+            Point corner_1 = (Point::Ones(boxSize) + Point::Basis(0, 1)) * PR_AMR_REFRATIO;
+            tags_1.initialize(f_tags_corner, dx_vect[1], corner_1);
+            h5.writeLevel(dx_vect[1], tags_1, "Tags_1");
+
+            grid.regrid(tags_1, 1, fineBoxSizeVect);
+
+            AMRData<double> data_before(grid, Point::Zeros());
+            for (int ii = 0; ii < 3; ii++)
+            {
+                data_before[ii].initialize(f_const, dx_vect[ii], ii);
+            }
+            h5.writeAMRData({"pre-nested"}, dx_vect[0], data_before, "Grids_0");
+
+            grid.enforceNesting(1, nestingDistance);
+
+            AMRData<double> data_after(grid, Point::Zeros());
+            for (int ii = 0; ii < 3; ii++)
+            {
+                data_after[ii].initialize(f_const, dx_vect[ii], ii);
+            }
+            h5.writeAMRData({"nested"}, dx_vect[0], data_after, "Grids_1");
+            break;
+            // ===========================================================
+            // TEST 11
+            } case 11: {
+            if (procID() == 0)
+            {
+                std::cout << "Running Test 10: Enforce Nesting At (Non)Periodic Boundaries" << std::endl;
+            }
+            int nestingDistance = args.get("nestingDistance");
+            double dx_vect[3];
+            dx_vect[0] = dx;
+            dx_vect[1] = dx_vect[0] / PR_AMR_REFRATIO;
+            dx_vect[2] = dx_vect[1] / PR_AMR_REFRATIO;
+
+            Box domain = Box::Cube(domainSize);
+
+            Point boxSizeVect = Point::Ones(boxSize);
+            Point fineBoxSizeVect = Point::Ones(boxSize / 2); 
+            std::array<bool, DIM> periodicity;
+            for (int ii = 0; ii < DIM; ii++)
+            {
+                if (ii == 0) { periodicity[ii] = true; }
+                else {periodicity[ii] = false; }
+            }
+            
+            if (procID() == 0)
+            {
+                std::cout << "\tBox Size (Coarse): " << boxSizeVect << std::endl;
+                std::cout << "\tBox Size (Fine): " << fineBoxSizeVect << std::endl;
+            }
+            ProblemDomain problemDomain(domain, periodicity);
+            DisjointBoxLayout layout(problemDomain, boxSizeVect);
+
+            AMRGrid grid(layout, 3);
+
+            LevelTagData tags_0(layout, Point::Zeros());
+            //Point corner_0 = Point::Basis(1, boxSize / (2*PR_AMR_REFRATIO)) + Point::Ones();
+            Point corner_0 = Point::Zeros();
+            tags_0.initialize(f_tags_corner, dx_vect[0], corner_0);
+            h5.writeLevel(dx_vect[0], tags_0, "Tags_0");
+
+            grid.regrid(tags_0, 0, boxSizeVect);
+
+            LevelTagData tags_1(grid[1], Point::Zeros());
+            //Point corner_1 = (Point::Basis(1, boxSize / (2*PR_AMR_REFRATIO)) + Point::Ones()) * PR_AMR_REFRATIO;
+            Point corner_1 = Point::Ones()*PR_AMR_REFRATIO;
+            tags_1.initialize(f_tags_corner, dx_vect[1], corner_1);
+            h5.writeLevel(dx_vect[1], tags_1, "Tags_1");
+
+            grid.regrid(tags_1, 1, fineBoxSizeVect);
+
+            AMRData<double> data_before(grid, Point::Zeros());
+            for (int ii = 0; ii < 3; ii++)
+            {
+                data_before[ii].initialize(f_const, dx_vect[ii], ii);
+            }
+            h5.writeAMRData({"pre-nested"}, dx_vect[0], data_before, "Grids_0");
+
+            grid.enforceNesting(1, nestingDistance);
+
+            AMRData<double> data_after(grid, Point::Zeros());
+            for (int ii = 0; ii < 3; ii++)
+            {
+                data_after[ii].initialize(f_const, dx_vect[ii], ii);
+            }
+            h5.writeAMRData({"nested"}, dx_vect[0], data_after, "Grids_1");
+            break;
             } default: {
             break;
             }
         } // End switch (TEST_NUM)
 
     } // End for (n)
-    /*
-       if (true)
-       {
-    // Create Finer Level Test
-    Box domain = Box::Cube(domainSize);
-
-    Point tagBufferSize = Point::Ones(bufferSize);
-    ProblemDomain problemDomain(domain, true);
-    DisjointBoxLayout layout(problemDomain, boxSizeVect);
-
-    AMRGrid grid(layout, 2);
-    LevelTagData tags(layout, tagBufferSize);
-    tags.initialize(f_tags_line, dx, origin);
-
-    h5.writeLevel(tags, "Tags_0"); 
-    AMRGrid::buffer(tags, bufferSize);
-    h5.writeLevel(tags, "Tags_1"); 
-
-    grid.regrid(tags, 0);
-
-    if (procID() == 0)
-    {
-    std::cout << "Created mesh is radially symmetric: " << grid[1].radialSymmetry(originPatch * PR_AMR_REFRATIO) << std::endl;
-    for (int ii = 0; ii < DIM; ii++)
-    {
-    std::cout << "Created mesh is symmetric in coordinate " << ii << ": " << grid[1].mirrorSymmetry(originPatch * PR_AMR_REFRATIO, ii) << std::endl;
-    }
-    }
-
-    AMRData<double> data(grid, Point::Zeros());
-    data.setToZero();
-
-    //LevelBoxData<double> data_0(grid[0], Point::Zeros());
-    //LevelBoxData<double> data_1(grid[1], Point::Zeros());
-
-    //data_0.setToZero();
-    //data_1.setToZero();
-
-    h5.writeLevel( 1.0, data[0], "Level_0");
-    h5.writeLevel( 1.0/PR_AMR_REFRATIO, data[1], "Level_1");
-    h5.writeAMRData({"data"}, 1.0, data, "AllLevels");
-
-    } else {
-    Point boxSizeVect = Point::Ones(boxSize);
-
-    double dx_0 = dx;
-    double dx_1 = dx_0 / PR_AMR_REFRATIO;
-    double dx_2 = dx_1 / PR_AMR_REFRATIO;
-
-    Box domain_0 = Box::Cube(domainSize);        
-    ProblemDomain problemDomain_0(domain_0, true);
-    DisjointBoxLayout layout_0(problemDomain_0, boxSizeVect);
-
-    AMRGrid grid(layout_0, 3);
-
-    LevelTagData tags_0(layout_0, Point::Zeros());
-    Point corner_0 = Point::Ones(boxSize);
-    tags_0.initialize(f_tags_corner, dx_0, corner_0);
-    h5.writeLevel(dx_0, tags_0, "Tags_0");
-
-    grid.regrid(tags_0, 0);
-
-    LevelTagData tags_1(grid[1], Point::Zeros());
-    Point corner_1 = Point::Ones(boxSize * PR_AMR_REFRATIO);
-    tags_1.initialize(f_tags_corner, dx_1, corner_1);
-    h5.writeLevel(dx_1, tags_1, "Tags_1");
-
-    grid.regrid(tags_1, 1);
-
-    LevelBoxData<double> data_L0_0(grid[0], Point::Zeros());
-    LevelBoxData<double> data_L1_0(grid[1], Point::Zeros());
-    LevelBoxData<double> data_L2_0(grid[2], Point::Zeros());
-
-    data_L0_0.setToZero();
-    data_L1_0.setToZero();
-    data_L2_0.setToZero();
-
-    h5.writeLevel(dx_0, data_L0_0, "Data_L0_0");
-    h5.writeLevel(dx_1, data_L1_0, "Data_L1_0");
-    h5.writeLevel(dx_2, data_L2_0, "Data_L2_0");
-
-    grid.enforceNesting(1);
-
-    LevelBoxData<double> data_L0_1(grid[0], Point::Zeros());
-    LevelBoxData<double> data_L1_1(grid[1], Point::Zeros());
-    LevelBoxData<double> data_L2_1(grid[2], Point::Zeros());
-
-    data_L0_1.setToZero();
-    data_L1_1.setToZero();
-    data_L2_1.setToZero();
-
-    h5.writeLevel(dx_0, data_L0_1, "Data_L0_1");
-    h5.writeLevel(dx_1, data_L1_1, "Data_L1_1");
-    h5.writeLevel(dx_2, data_L2_1, "Data_L2_1");
-}
-*/
 #ifdef PR_MPI
     MPI_Finalize();
 #endif
