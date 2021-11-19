e9f290a99060df593c7ea21ab6536cb623284d26
Daniel Waters <dwaters@lbl.gov>
2021-11-19 00:22:36 -0500

    need to get nvToolsExt

--- .github/workflows/dependencies/cuda.sh
+++ .github/workflows/dependencies/cuda.sh
@@ -11,6 +11,6 @@ echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x8
     | sudo tee /etc/apt/sources.list.d/cuda.list
 
 sudo apt-get -qq update
-sudo apt-get install -y cuda-compiler-11-2
+sudo apt-get install -y cuda-compiler-11-2 cuda-command-line-tools-11-2 cuda-nvtx-11-2
 
 sudo ln -s cuda-11.2 /usr/local/cuda
