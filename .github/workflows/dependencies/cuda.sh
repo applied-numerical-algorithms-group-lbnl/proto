4ba977e80bb48e5c97ba414e2136dad4ddd52ef6
Daniel Waters <dwaters@lbl.gov>
2021-11-23 18:26:31 -0500

    might need to install more

--- .github/workflows/dependencies/cuda.sh
+++ .github/workflows/dependencies/cuda.sh
@@ -11,6 +11,12 @@ echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x8
     | sudo tee /etc/apt/sources.list.d/cuda.list
 
 sudo apt-get -qq update
-sudo apt-get install -y cuda-compiler-11-0 cuda-nvtx-11-0 cuda-runtime-11-0
+sudo apt-get install -y \
+    cuda-command-line-tools-11-0 \
+    cuda-compiler-11-0 \
+    cuda-cupti-dev-11-0 \
+    cuda-minimal-build-11-0 \
+    cuda-nvtx-11-0 \
+    cuda-runtime-11-0
 
 sudo ln -s cuda-11.0 /usr/local/cuda
