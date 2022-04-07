a279daf190d1c71ef996482f9c13eef0df2d11d6
Chris L. Gebhart <cgebhart@lbl.gov>
2022-04-07 15:02:52 -0700

    LevelData::copyTo now works as long as there are now ghost cells. LevelData::exchange is still broken. Copier needs to be reworked to know about MemType directly.

--- examples/DevTests/exec/_tests/LevelData_Exchange.cpp
+++ examples/DevTests/exec/_tests/LevelData_Exchange.cpp
@@ -1,94 +1,131 @@
 #include "Proto.H"
-#include "../../common/InputParser.H"
-#include "func.H"
+#include "InputParser.H"
+
+#define NUMCOMPS 1
 
 using namespace Proto;
 
-void f_clearExterior(Point& a_pt, Var<double>& a_dst, Var<double>& a_src, Box a_box)
+PROTO_KERNEL_START
+void
+f_rampHostF(const Point& a_pt, Var<int,NUMCOMPS, HOST>& a_data)
 {
-    if (a_box.contains(a_pt))
+    Point x = a_pt + Point::Ones();
+    for (int comp = 0; comp < NUMCOMPS; comp++)
     {
-        a_dst(0) = a_src(0);
-    } else {
-        a_dst(0) = 0;
+        a_data(comp) = (comp+1)*10 + x[0];
+#if DIM > 1
+        a_data(comp) = (comp+1)*100 + 10*x[0] + x[1];
+#endif
+#if DIM > 2
+        a_data(comp) = (comp+1)*1000 + 100*x[0] + 10*x[1] + x[2];
+#endif
     }
 }
+PROTO_KERNEL_END(f_rampHostF, f_rampHost);
 
-
+PROTO_KERNEL_START
+void
+f_rampDeviF(const Point& a_pt, Var<int,NUMCOMPS, DEVICE>& a_data)
+{
+    Point x = a_pt + Point::Ones();
+    for (int comp = 0; comp < NUMCOMPS; comp++)
+    {
+        a_data(comp) = (comp+1)*10 + x[0];
+#if DIM > 1
+        a_data(comp) = (comp+1)*100 + 10*x[0] + x[1];
+#endif
+#if DIM > 2
+        a_data(comp) = (comp+1)*1000 + 100*x[0] + 10*x[1] + x[2];
+#endif
+    }
+}
+PROTO_KERNEL_END(f_rampDeviF, f_rampDevi);
 
 int main(int argc, char** argv)
 {
+#ifdef PR_MPI
+    MPI_Init(&argc, &argv);
+#endif
+
     HDF5Handler h5;
 
-    int domainSize = 32;
-    int boxSize = 8;
+    int domainSize = 64;
+    int boxSize = 16;
     int ghostSize = 1;
     std::array<bool, DIM> periodicity;
+    int testNum = 0;
+    for (int dir = 0; dir < DIM; dir++) { periodicity[dir] = true; }
 
     InputArgs args;
-    args.parse();
-    args.set("domainSize", &domainSize);
-    args.set("boxSize", &boxSize);
-    args.set("periodic_x", &periodicity[0]);
-    args.set("periodic_y", &periodicity[1]);
+    args.add("domainSize",      domainSize);
+    args.add("boxSize",         boxSize);
+    args.add("ghostSize",       ghostSize);
+    args.add("testNum",         testNum);
+    args.add("periodic_x",      periodicity[0]);
+    args.add("periodic_y",      periodicity[1]);
+#if DIM > 2
+    args.add("periodic_z",      periodicity[2]);
+#endif
+    args.parse(argc, argv);
+    args.print();
 
-    double k = 1;
-    double dx = 1.0 / domainSize;
+    std::array<double, DIM> dx;
+    dx.fill(1.0);
 
     Point boxSizeV = Point::Ones(boxSize);
     Box domainBox = Box::Cube(domainSize);
     ProblemDomain domain(domainBox, periodicity);
     DisjointBoxLayout layout(domain, boxSizeV);
 
-    LevelBoxData<double> srcData(layout, Point::Ones(ghostSize));
-    LevelBoxData<double> dstData(layout, Point::Ones(ghostSize));
-    LevelBoxData<double> errData(layout, Point::Ones(ghostSize));
-
-    errData.setToZero(); 
-    srcData.initialize(f_wave, dx, k); 
-    for (auto iter = layout.begin(); iter.ok(); ++iter)
+    if (testNum == 0)
     {
-        auto& src_i = srcData[*iter];
-        auto& dst_i = dstData[*iter];
-        Box   box_i = iter.box();
-        forallInPlace_p(f_clearExterior, dst_i, src_i, box_i);
-    }
-
-    h5.writeLevel(dx, srcData, "SRC");
-    h5.writeLevel(dx, dstData, "DST_0");
-
-    dstData.exchange();
-
-    h5.writeLevel(dx, dstData, "DST_1");
-
-    Box validDomainBox = domainBox;
-    for (int dir = 0; dir < DIM; dir++)
-    {
-        if (periodicity[dir]) 
+        if (procID() == 0)
         {
-            validDomainBox = validDomainBox.grow(Point::Basis(dir, ghostSize));
+            std::cout << "RUNNING TEST 0: MEMTYPE = HOST" << std::endl;
+        }
+        LevelBoxData<double, NUMCOMPS, HOST> srcData(layout, Point::Zeros());
+        LevelBoxData<double, NUMCOMPS, HOST> dstData(layout, Point::Ones(ghostSize));
+        LevelBoxData<double, NUMCOMPS, HOST> errData(layout, Point::Ones(ghostSize));
+
+        dstData.setVal(-1);
+        srcData.setVal(42);
+        errData.setVal(42);
+        //srcData.initialize(f_rampHost); 
+        //errData.initialize(f_rampHost);
+        for (auto iter = layout.begin(); iter.ok(); ++iter)
+        {
+            auto& src_i = srcData[*iter];
+            auto& dst_i = dstData[*iter];
+            src_i.copyTo(dst_i);
+        }
+        
+        h5.writeLevel(dx, srcData, "SRC");
+        h5.writeLevel(dx, dstData, "DST_0");
+
+        dstData.exchange();
+
+        h5.writeLevel(dx, dstData, "DST_1");
+
+        Reduction<double, Abs> rxn;
+        for (auto iter = layout.begin(); iter.ok(); ++iter)
+        {
+            BoxData<double, NUMCOMPS, HOST>& dst_i = dstData[*iter];
+            BoxData<double, NUMCOMPS, HOST>& err_i = errData[*iter];
+
+            err_i -= dst_i;
+            err_i.absMax(rxn);
+        }
+
+        h5.writeLevel(dx, errData, "ERROR");
+
+        if (rxn.fetch() < 1.0e-12) 
+        {
+            std::cout << "Error: " << rxn.fetch() << " | TEST PASSES." << std::endl;
+        } else {
+            std::cout << "Error: " << rxn.fetch() << " | TEST FAILS."  << std::endl;
         }
     }
-    Reduction<double, Abs> rxn;
-    for (auto iter = layout.begin(); iter.ok(); ++iter)
-    {
-        auto& src_i = srcData[*iter];
-        auto& dst_i = dstData[*iter];
-        auto& err_i = errData[*iter];
-
-        dst_i.copyTo(err_i);
-        err_i -= src_i;
-        forallInPlace_p(f_clearExterior, err_i, err_i, validDomainBox);
-        
-        err_i.absMax(rxn);
-    }
-
-    h5.writeLevel(dx, errData, "ERROR");
-
-    if (rxn.fetch() < 1.0e-12) 
-    {
-        std::cout << "Error: " << rxn.fetch() << " | TEST PASSES." << std::endl;
-    } else {
-        std::cout << "Error: " << rxn.fetch() << " | TEST FAILS."  << std::endl;
-    }
+#ifdef PR_MPI
+    MPI_Finalize();
+#endif
 }
