a76393841742e08e6253a3ae08122428ebeda994
Chris L. Gebhart <cgebhart@lbl.gov>
2022-04-18 14:24:14 -0700

    updated Reduction test code.

--- examples/DevTests/exec/_tests/Reduction_Reduce.cpp
+++ examples/DevTests/exec/_tests/Reduction_Reduce.cpp
@@ -21,13 +21,37 @@ int main(int argc, char** argv)
     Box domainBox = Box::Cube(domainSize);
     BoxData<int, 1, DEVICE> data(domainBox);
     data.setVal(1);
-
-    Reduction<int, Sum, DEVICE> rxn;
-
-    rxn.reduce(data.data(), data.size());
     
+#if 0
+    Reduction<int, Sum, DEVICE> rxn;
+    int sum = 0;
+    rxn.reduce(data.data(), data.size());
+    sum += rxn.fetch();
+    rxn.reset();
+    std::cout << "Sum: " << sum << " | Should be: " << domainBox.size() << std::endl;
+    rxn.reduce(data.data(), data.size());
+    sum += rxn.fetch();
+    rxn.reset();
+    std::cout << "Sum: " << sum << " | Should be: " << domainBox.size()*2 << std::endl;
+    rxn.reduce(data.data(), data.size());
+    sum += rxn.fetch();
+    rxn.reset();
+    std::cout << "Sum: " << sum << " | Should be: " << domainBox.size()*3 << std::endl;
+    rxn.reduce(data.data(), data.size());
+    sum += rxn.fetch();
+    rxn.reset();
+    std::cout << "Sum: " << sum << " | Should be: " << domainBox.size()*4 << std::endl;
+#else
+    Reduction<int, Sum, DEVICE> rxn;
+    rxn.reduce(data.data(), data.size());
     std::cout << "Sum: " << rxn.fetch() << " | Should be: " << domainBox.size() << std::endl;
-
+    rxn.reduce(data.data(), data.size());
+    std::cout << "Sum: " << rxn.fetch() << " | Should be: " << domainBox.size()*2 << std::endl;
+    rxn.reduce(data.data(), data.size());
+    std::cout << "Sum: " << rxn.fetch() << " | Should be: " << domainBox.size()*3 << std::endl;
+    rxn.reduce(data.data(), data.size());
+    std::cout << "Sum: " << rxn.fetch() << " | Should be: " << domainBox.size()*4 << std::endl;
+#endif
     #ifdef PR_MPI
     MPI_Finalize();
     #endif
