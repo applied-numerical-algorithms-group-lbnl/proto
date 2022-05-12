a19f0546f186cd2ea625b34cfd90f13bd83d7b84
Chris <cgebhart@lbl.gov>
2022-05-12 08:59:30 -0700

    Successfully refactored BlockGraph.
    BlockGraphNodes now have the requisite information to form high-codimensional boundaries correctly when the graph is built. Adjacency and Connectivity XPoint tests are both passing in 2D and 3D.

--- tests/BlockGraphTests.cpp
+++ tests/BlockGraphTests.cpp
@@ -17,80 +17,7 @@ BlockGraph buildXPoint()
     return graph;
 }
 
-TEST(BlockGraph, BasicFunctions) {
-    CoordPermutation R;
-    BlockGraphNode block(0);
-    int numBlocks = 3;
-    BlockGraph graph(numBlocks);
-    EXPECT_EQ(graph.size(), numBlocks);
-    for (int ii = 0; ii < numBlocks; ii++)
-    {
-        EXPECT_EQ(graph[ii].id(), ii);
-    }
-}
-
-TEST(BlockGraph, PlanarXPoint) {
-    int numBlocks = XPOINT_SIZE;
-    BlockGraph graph = buildXPoint();
-    Box K = Box::Kernel(1);
-    for (int ii = 0; ii < numBlocks; ii++)
-    {
-        const BlockGraphNode& block = graph[ii];
-        for (auto dir : K)
-        {
-#if DIM == 2
-            if (dir == Point::Basis(0) || dir == Point::Basis(0) + Point::Basis(1,-1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 1);
-                EXPECT_EQ(block.boundaries(dir)[0]->id(), (ii+1)%numBlocks);
-            } else if(dir == Point::Basis(1) || dir == Point::Basis(0,-1) + Point::Basis(1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 1);
-                EXPECT_EQ(block.boundaries(dir)[0]->id(), (ii+numBlocks-1) % numBlocks);
-            } else if (dir == Point::Basis(0) + Point::Basis(1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 2);
-            } else {
-                EXPECT_EQ(block.numBoundaries(dir), 0);
-            }
-#elif DIM == 3
-            if (
-                    dir == Point::Basis(0)
-                 || dir == Point::Basis(0) + Point::Basis(2,+1)
-                 || dir == Point::Basis(0) + Point::Basis(2,-1)
-                 || dir == Point::Basis(0) + Point::Basis(1,-1)
-                 || dir == Point::Basis(0) + Point::Basis(1,-1) + Point::Basis(2,+1)
-                 || dir == Point::Basis(0) + Point::Basis(1,-1) + Point::Basis(2,-1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 1);
-                EXPECT_EQ((*block.boundaries(dir).begin())->id(), (ii+1)%numBlocks);
-            } else if(
-                    dir == Point::Basis(1)
-                 || dir == Point::Basis(1) + Point::Basis(2,+1)
-                 || dir == Point::Basis(1) + Point::Basis(2,-1)
-                 || dir == Point::Basis(1) + Point::Basis(0,-1)
-                 || dir == Point::Basis(1) + Point::Basis(0,-1) + Point::Basis(2,+1)
-                 || dir == Point::Basis(1) + Point::Basis(0,-1) + Point::Basis(2,-1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 1);
-                EXPECT_EQ((*block.boundaries(dir).begin())->id(), (ii+numBlocks-1) % numBlocks);
-            } else if (
-                    dir == Point::Basis(0) + Point::Basis(1)
-                 || dir == Point::Basis(0) + Point::Basis(1) + Point::Basis(2,1)
-                 || dir == Point::Basis(0) + Point::Basis(1) + Point::Basis(2,-1))
-            {
-                EXPECT_EQ(block.numBoundaries(dir), 2);
-            } else {
-                EXPECT_EQ(block.numBoundaries(dir), 0);
-            }
-#else
-            EXPECT_TRUE(false)
-#endif
-        }
-    }
-}
-
-TEST(BlockGraph, Adjacent) {
+TEST(BlockGraph, XPointAdjacent) {
     int numBlocks = XPOINT_SIZE;
     BlockGraph graph = buildXPoint();
     for (int ii = 0; ii < numBlocks; ii++)
@@ -114,7 +41,7 @@ TEST(BlockGraph, Adjacent) {
     }
 }
 
-TEST(BlockGraph, Connectivity) {
+TEST(BlockGraph, XPointConnectivity) {
     int numBlocks = XPOINT_SIZE;
     BlockGraph graph = buildXPoint();
     for (int src = 0; src < numBlocks; src++)
@@ -122,19 +49,13 @@ TEST(BlockGraph, Connectivity) {
         for (int dst = 0; dst < numBlocks; dst++)
         {
             auto connectivity = graph.connectivity(src, dst);
-            std::cout << "Checking connectivity from " << src << " to " << dst << std::endl;
-            for (auto item : connectivity)
-            {
-                std::cout << item << ", ";
-            }
-            std::cout << std::endl;
             Box dirs;
-            if (src == dst)
+            if (dst == src)
             {
                 // do nothing
-            } else if (dst - src == 1) {
+            } else if (dst == (src+1)%numBlocks) {
                 dirs = Box::Kernel(1).grow(0, -1).shift(Point::Basis(0));
-            } else if (src - dst == 1) {
+            } else if (dst == (src+numBlocks-1)%numBlocks) {
                 dirs = Box::Kernel(1).grow(1, -1).shift(Point::Basis(1));
             } else {
                 dirs = Box::Kernel(1).edge(Point::Basis(0) + Point::Basis(1));
