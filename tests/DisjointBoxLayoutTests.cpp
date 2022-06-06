f1989c02a0c1716b3d62f88d65c20f844506c7a9
Chris <cgebhart@lbl.gov>
2022-06-06 16:47:04 -0700

    Completed Rework of DisjointBoxLayout
    DisjointBoxLayout can now be dynamically load balanced using the new utility functions DisjointBoxLayout::loadBalance and DisjointBoxLayout::loadAssign
    
    This refactor includes the removal of DBLInternals which has been replaced with BoxPartition. This new object expands on the utility of DBLInternals and adds additional utility. All classes in Proto that previously depended on DBLInternals (e.g. DataIterator) now properly reference BoxPartition instead
    
    New CI tests have been written for DisjointBoxLayout to assist with this rework and generally improve the quality of Proto's CI test suite
    
    All currently written CI tests pass in serial and parallel. GPU tests have not been run, but are not expected to be impacted by these changes.

--- tests/DisjointBoxLayoutTests.cpp
+++ tests/DisjointBoxLayoutTests.cpp
@@ -2,7 +2,6 @@
 #include "Proto.H"
 
 using namespace Proto;
-
 TEST(DisjointBoxLayout, Iteration) {
     int domainSize = 64;
     int boxSize = 16;
@@ -13,21 +12,36 @@ TEST(DisjointBoxLayout, Iteration) {
     ProblemDomain domain(domainBox, periodicity);
     DisjointBoxLayout layout(domain, boxSizeVect);
 
+    std::set<Box> correctBoxSet;
+    Box patches = domainBox.coarsen(boxSizeVect);
+    for (auto p : patches)
+    {
+        Box b(p,p);
+        correctBoxSet.insert(b.refine(boxSizeVect));
+    }
+
     int N = pow(domainSize / boxSize, DIM);
     int n0 = N / numProc();
     int r = N % numProc();
     if (procID() < r) { n0++; }
     int n = 0;
+    std::set<Box> boxSet;
     for (auto index : layout)
     {
-        //pout() << "patch: " << layout[index] << " | local: " << index.local() << " | global: " << index.global() << std::endl;
         n++;
+        Box b = layout[index];
+        EXPECT_EQ(b.sizes(), boxSizeVect);
+        EXPECT_EQ(boxSet.find(b), boxSet.end());
+        EXPECT_FALSE(correctBoxSet.find(b) == correctBoxSet.end());
+        for (auto bi : boxSet)
+        {
+            EXPECT_TRUE((bi & b).empty());
+        }
+        boxSet.insert(b);
     }
-    //pout() << "Iteration Test | N: " << N << " | n: " << n << std::endl;
     EXPECT_EQ(n, n0);
 }
 
-#ifdef PR_MPI
 TEST(DisjointBoxLayout, LoadBalance) {
     int domainSize = 64;
     int boxSize = 16;
@@ -39,28 +53,126 @@ TEST(DisjointBoxLayout, LoadBalance) {
     DisjointBoxLayout layout_0(domain, boxSizeVect);
     DisjointBoxLayout layout_1(domain, boxSizeVect);
     
+    std::set<Box> correctBoxSet;
+    Box patches = domainBox.coarsen(boxSizeVect);
+    for (auto p : patches)
+    {
+        Box b(p,p);
+        correctBoxSet.insert(b.refine(boxSizeVect));
+    }
+
     int N = numProc();
     int r = procID();
     int N0 = (N % 2 == 0) ? N / 2 : N / 2 + 1;
     int N1 = N / 2;
     int midRank = N0;
-    //std::cout << "N0: " << N0 << " | N1: " << N1 << " | midRank: " << midRank << std::endl;
     layout_0.loadBalance(0, midRank);
     layout_1.loadBalance(midRank, N);
+   
+    int numBoxes = pow(domainSize / boxSize, DIM);
+    int num_0 = (numBoxes / midRank);
+    int rem_0 = (numBoxes % midRank);
+    num_0 = procID() < rem_0 ? num_0+1 : num_0;
+    if (procID() >= midRank) { num_0 = 0; }
 
-    /*
+    int num_1 = 0;
+    if (N > 1)
+    {
+        num_1 = (numBoxes / (N - midRank));
+        int rem_1 = (numBoxes % (N - midRank));
+        num_1 = procID() < rem_1 ? num_1+1 : num_1;
+    }
+    if (procID() < midRank) { num_1 = 0; }
+
+    int n0 = 0;
+    int n1 = 0;
+    std::set<Box> boxSet_0;
+    std::set<Box> boxSet_1;
     for (auto iter : layout_0)
     {
-        pout() << "layout_0 | local: " << iter.local() << " | global: " << iter.global() << " | box: " << layout_0[iter] << std::endl;
+        n0++;
+        Box b = layout_0[iter];
+        EXPECT_EQ(b.sizes(), boxSizeVect);
+        EXPECT_EQ(boxSet_0.find(b), boxSet_0.end());
+        EXPECT_FALSE(correctBoxSet.find(b) == correctBoxSet.end());
+        for (auto bi : boxSet_0)
+        {
+            EXPECT_TRUE((bi & b).empty());
+        }
+        boxSet_0.insert(b);
     }
     for (auto iter : layout_1)
     {
-        pout() << "layout_1 | local: " << iter.local() << " | global: " << iter.global() << " | box: " << layout_1[iter] << std::endl;
+        n1++;
+        Box b = layout_1[iter];
+        EXPECT_EQ(b.sizes(), boxSizeVect);
+        EXPECT_EQ(boxSet_1.find(b), boxSet_1.end());
+        EXPECT_FALSE(correctBoxSet.find(b) == correctBoxSet.end());
+        for (auto bi : boxSet_0)
+        {
+            EXPECT_TRUE((bi & b).empty());
+        }
+        boxSet_1.insert(b);
     }
-    */
-
+    EXPECT_EQ(n0, num_0);
+    EXPECT_EQ(n1, num_1);
 }
-#endif
+
+TEST(DisjointBoxLayout, LoadAssign) {
+    int domainSize = 64;
+    int boxSize = 16;
+    Box domainBox = Box::Cube(domainSize);
+    Point boxSizeVect = Point::Ones(boxSize);
+    std::array<bool, DIM> periodicity;
+    periodicity.fill(true);
+    ProblemDomain domain(domainBox, periodicity);
+    DisjointBoxLayout layout(domain, boxSizeVect);
+    
+    std::set<Box> correctBoxSet;
+    Box patches = domainBox.coarsen(boxSizeVect);
+    for (auto p : patches)
+    {
+        Box b(p,p);
+        correctBoxSet.insert(b.refine(boxSizeVect));
+    }
+
+    unsigned int numBoxes = pow(domainSize / boxSize, DIM);
+    unsigned int numUnassigned = numBoxes;
+    std::vector<std::pair<int, unsigned int>> procAssign;
+    procAssign.resize(numProc());
+    for (int ii = 0; ii < numProc(); ii++)
+    {
+        unsigned int assignNum = min(ii+1, (int)numUnassigned);
+        procAssign[ii] = std::pair<int, unsigned int>(ii, assignNum);
+        numUnassigned -= assignNum;
+    }
+    procAssign[numProc()-1].second += numUnassigned;
+    layout.loadAssign(procAssign);
+    layout.print();
+    int n0 = 0;
+    for (auto data : procAssign)
+    {
+        if (data.first == procID()) { n0 = data.second; }
+    }
+
+    int n = 0;
+    std::set<Box> boxSet;
+    for (auto ind : layout)
+    {
+        Box b = layout[ind];
+        EXPECT_EQ(b.sizes(), boxSizeVect);
+        EXPECT_EQ(boxSet.find(b), boxSet.end());
+        EXPECT_FALSE(correctBoxSet.find(b) == correctBoxSet.end());
+        for (auto bi : boxSet)
+        {
+            EXPECT_TRUE((bi & b).empty());
+        }
+        boxSet.insert(b);
+        n++;
+    }
+    EXPECT_EQ(n, n0);
+}
+
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
 #ifdef PR_MPI
