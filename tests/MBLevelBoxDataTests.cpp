84ccac66df678fac8851c3e0565a136dc0c23b3b
Chris <cgebhart@lbl.gov>
2022-07-22 14:52:59 -0700

    Finished initial implementation of MBLevelBoxData definition
    [FEATURE] MBLevelBoxData are now defined with MBBoundaryData which will hold intermediate values during data exchange. MBBoundaryData are currently defined for each BLOCK-BLOCK boundary and are duplicated on every parallel process. It is likely that MBBoundaryData should be defined on PATCH-PATCH boundaries instead. This implementation will be explored in the next iteration.

--- tests/MBLevelBoxDataTests.cpp
+++ tests/MBLevelBoxDataTests.cpp
@@ -20,7 +20,7 @@ MBProblemDomain buildXPoint(int a_domainSize)
     return domain;
 }
 
-TEST(SUITE_NAME, TEST_NAME) {
+TEST(MBLevelBoxData, Construction) {
     int domainSize = 64;
     int boxSize = 16;
     int ghostSize = 1;
@@ -30,37 +30,62 @@ TEST(SUITE_NAME, TEST_NAME) {
 
     MBLevelBoxData<int, NCOMP, HOST> hostData(layout, ghostSize);
    
-    /*
+    Point nx = Point::Basis(0);
+    Point ny = Point::Basis(1);
+   
+    int numTests = 0;
     for (int bi = 0; bi < XPOINT_SIZE; bi++)
     {
         auto blockLayout = layout.layout(bi);
         Box blockDomainBox = blockLayout.domain().box();
-
-        for (auto iter : layout)
+        Box K = Box::Kernel(1);
+        for (auto dir : K)
         {
-            auto localIndex = layout.blockIndex(iter);
-            auto& patch = hostData[iter];
-            Point patchID = blockLayout.point(localIndex);
-            Box K = Box::Kernel(1);
-            for (auto dir : K)
+            if (dir == Point::Zeros()) { continue; }
+            auto bounds = hostData.bounds(bi, dir);
+            Box localBoundBox = blockDomainBox.adjacent(dir,ghostSize);
+            if (bounds.size() > 0)
             {
-                if (dir == Point::Zeros()) { continue; }
-                Box edgeBox = blockDomainBox.edge(dir, 1);
-                if (edgeBox.contains(patchID))
+                EXPECT_TRUE(dir == nx || dir == ny || dir == nx+ny);
+                for (auto b : bounds)
                 {
-                    Box localBox = patch.box().adjacent(dir,1);
-                    auto bounds = domain.graph().boundaries(bi, dir);
-                    for (auto bound : bounds)
-                    {
-                        auto adjBlock = bound.dstBlock;
-                        auto adjBox = domain.convert(localBox, bi, adjBlock);
-
-                    }
+                    EXPECT_EQ(b.localBlock, bi);
+                    EXPECT_NE(b.adjBlock, bi);
+                    //std::cout << b.localData->box() << " == " << localBoundBox << std::endl;
+                    EXPECT_EQ(b.localData->box(), localBoundBox);
                 }
+                unsigned int xBlock = (bi+1) % XPOINT_SIZE;
+                unsigned int yBlock = (bi-1+XPOINT_SIZE) % XPOINT_SIZE;
+                if (dir == nx)
+                {
+                    Box adjBoundBox = domain.convert(localBoundBox, bi, xBlock);
+                    EXPECT_EQ(bounds.size(), 1);
+                    EXPECT_EQ(bounds[0].adjBlock, xBlock);
+                    //std::cout << bounds[0].adjData->box() << " == " << adjBoundBox << std::endl;
+                    EXPECT_EQ(bounds[0].adjData->box(), adjBoundBox);
+                } else if (dir == ny)
+                {
+                    Box adjBoundBox = domain.convert(localBoundBox, bi, yBlock);
+                    EXPECT_EQ(bounds.size(), 1);
+                    EXPECT_EQ(bounds[0].adjBlock, yBlock);
+                    //std::cout << bounds[0].adjData->box() << " == " << adjBoundBox << std::endl;
+                    EXPECT_EQ(bounds[0].adjData->box(), adjBoundBox);
+                } else if (dir == nx+ny)
+                {
+                    EXPECT_EQ(bounds.size(), max(XPOINT_SIZE-3, 0));
+                    for (auto b : bounds)
+                    {
+                        EXPECT_NE(b.adjBlock, xBlock);
+                        EXPECT_NE(b.adjBlock, yBlock);
+                        Box cornerBox = Box::Cube(domainSize).edge(nx+ny, ghostSize);
+                        //std::cout << b.adjData->box() << " == " << cornerBox << std::endl;
+                        EXPECT_EQ(b.adjData->box(), cornerBox);
+                    }
+                } 
             }
-
         }
     }
+    /*
     hostData.initialize(f_MBPointID);
     for (auto iter : layout)
     {
