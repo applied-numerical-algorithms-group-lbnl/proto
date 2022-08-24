9a59c583f1eee192b55094096779b5c1fc62afc7
Chris <cgebhart@lbl.gov>
2022-08-24 11:54:10 -0700

    PolarExchangeCopier now appears to be working correctly

--- examples/PolarExchangeCopier/exec/main.cpp
+++ examples/PolarExchangeCopier/exec/main.cpp
@@ -8,12 +8,21 @@ template<typename T, unsigned int C, MemType MEM>
     PROTO_KERNEL_START 
 void f_initialize_(Point& a_pt, Var<T, C, MEM>& a_U, double a_dx)
 {
+    //Point x = a_pt + Point::Ones();
     double x = a_pt[0]*a_dx + a_dx/2.0;
     double y = a_pt[1]*a_dx + a_dx/2.0;
     a_U(0) = sin(2.0*M_PI*x)*sin(8.0*M_PI*y);
+    //for (int cc = 0; cc < C; cc++)
+    //{
+    //    a_U(cc) = (cc+1) + 1e-2*x[0] + 1e-4*x[1];
+    //}
 #if DIM > 2
     double z = a_pt[2]*a_dx + a_dx/2.0;
     a_U(0) *= sin(4.0*M_PI*z);
+    //for (int cc = 0; cc < C; cc++)
+    //{
+    //    a_U(cc) += 1e-6*x[2];
+    //}
 #endif
 }
 PROTO_KERNEL_END(f_initialize_, f_initialize)
@@ -50,7 +59,6 @@ int main(int argc, char** argv)
     h5.writeLevel(dx, data, "DATA_0");
     data.exchange();
     h5.writeLevel(dx, data, "DATA_1");
-
 #ifdef PR_MPI
     MPI_Finalize();
 #endif
