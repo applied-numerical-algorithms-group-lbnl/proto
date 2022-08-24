06484789138fa32f4147fb4580492533498773e3
Chris <cgebhart@lbl.gov>
2022-08-24 13:34:30 -0700

    Hopefully fixed an issue that was preventing LevelCopierOp from operating correctly between data holders with more than one component.

--- examples/PolarExchangeCopier/exec/main.cpp
+++ examples/PolarExchangeCopier/exec/main.cpp
@@ -8,21 +8,27 @@ template<typename T, unsigned int C, MemType MEM>
     PROTO_KERNEL_START 
 void f_initialize_(Point& a_pt, Var<T, C, MEM>& a_U, double a_dx)
 {
-    //Point x = a_pt + Point::Ones();
+#if 0
     double x = a_pt[0]*a_dx + a_dx/2.0;
     double y = a_pt[1]*a_dx + a_dx/2.0;
     a_U(0) = sin(2.0*M_PI*x)*sin(8.0*M_PI*y);
-    //for (int cc = 0; cc < C; cc++)
-    //{
-    //    a_U(cc) = (cc+1) + 1e-2*x[0] + 1e-4*x[1];
-    //}
+#else
+    Point x = a_pt + Point::Ones();
+    for (int cc = 0; cc < C; cc++)
+    {
+        a_U(cc) = (cc+1) + 1e-2*x[0] + 1e-4*x[1];
+    }
+#endif
 #if DIM > 2
+#if 0
     double z = a_pt[2]*a_dx + a_dx/2.0;
     a_U(0) *= sin(4.0*M_PI*z);
-    //for (int cc = 0; cc < C; cc++)
-    //{
-    //    a_U(cc) += 1e-6*x[2];
-    //}
+#else
+    for (int cc = 0; cc < C; cc++)
+    {
+        a_U(cc) += 1e-6*x[2];
+    }
+#endif
 #endif
 }
 PROTO_KERNEL_END(f_initialize_, f_initialize)
@@ -35,8 +41,8 @@ int main(int argc, char** argv)
 #endif
 
     // DEFAULT PARAMETERS
-    int domainSize = 64;
-    int boxSize = 16;
+    int domainSize = 8;
+    int boxSize = 4;
     
     // PARSE COMMAND LINE
     InputArgs args;
@@ -53,12 +59,16 @@ int main(int argc, char** argv)
     per.fill(true);
     ProblemDomain pd(domain,per);
     DisjointBoxLayout layout(pd,Point::Ones(boxSize));
-    LevelBoxData<double> data(layout, Point::Ones(4));
+    LevelBoxData<double, 2> data(layout, Point::Ones(2));
     data.defineExchange<PolarExchangeCopier>(0,1);
     data.initialize(f_initialize, dx); 
     h5.writeLevel(dx, data, "DATA_0");
     data.exchange();
     h5.writeLevel(dx, data, "DATA_1");
+    for (auto iter : layout)
+    {
+        //data[iter].printData(2*DIM);
+    }
 #ifdef PR_MPI
     MPI_Finalize();
 #endif
