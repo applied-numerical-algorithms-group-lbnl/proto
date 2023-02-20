2d0af299df6df6c5b25532ed50f973cf19ba1080
Phillip Colella <pcolella@lbl.gov>
2023-02-20 14:11:48 -0800

    (1) Fixed AMRRK4 bugs - can run single level; (2) added Richardson error capability (maxRefs < 1); (3) output pass / fail tests

--- examples/AMRAdvection/exec/main.cpp
+++ examples/AMRAdvection/exec/main.cpp
@@ -52,11 +52,11 @@ void f_advectionExactF(
         double r = sqrt(rsq);
         if (r > r0)
         {
-            a_U(comp) = 1.;
+            a_U(comp) = 0.;
         }
         else
         {
-            a_U(comp) = 1.+pow(cos(M_PI*r/r0/2),6);
+            a_U(comp) = pow(cos(M_PI*r/r0/2),6);
         }
     }
 }
@@ -76,12 +76,13 @@ int main(int argc, char** argv)
     int domainSize = 32;
     int boxSize = 32;
     int refRatio = 4;
-    int maxTimesteps = 64;
+    int maxTimesteps = 4096;
     int numLevels = 3;
     int regridBufferSize = 2;
     double maxTime = 1.0;
     int outputInterval = 1;
     double t0 = 0.0;
+    int maxRefs = 2;
     //std::array<bool, DIM> periodicity;
     Array<bool, DIM> periodicity;
     periodicity.fill(true);
@@ -98,123 +99,181 @@ int main(int argc, char** argv)
     args.add("numLevels",       numLevels);
     args.add("regridBufferSize",regridBufferSize);
     args.add("t0",              t0);
+    args.add("maxRefs",         maxRefs);
     args.parse(argc, argv);
-    args.print();
+    std::vector<double> errorRefs;
+    double consError;
+    for (int numRef = 0;numRef < maxRefs;numRef++)
+      {
+        args.print();
 
-    if (boxSize > domainSize)
-    {
-        pout() << "Input error: boxSize > domainSize. Forcing boxSize == domainSize." << std::endl;
-        boxSize = domainSize;
-    }
-    PR_TIMER_SETFILE(to_string(domainSize) 
-        + ".DIM=" + to_string(DIM) + ".numProc=" + to_string(numProc())
-        + "AMRAdvection.time.table");
+        if (boxSize > domainSize)
+          {
+            pout() << "Input error: boxSize > domainSize. Forcing boxSize == domainSize." << std::endl;
+            boxSize = domainSize;
+          }
+        PR_TIMER_SETFILE(to_string(domainSize) 
+                         + ".DIM=" + to_string(DIM) + ".numProc=" + to_string(numProc())
+                         + "AMRAdvection.time.table");
 
-    Point boxSizeVect = Point::Ones(boxSize);
-    Box domainBox = Box::Cube(domainSize);
-    ProblemDomain domain(domainBox, periodicity);
-    DisjointBoxLayout layout(domain, boxSizeVect);
+        ofstream sizeout(to_string(domainSize) 
+                         + ".DIM=" + to_string(DIM) + ".numProc=" + to_string(numProc())
+                         + "HWM.curve");
+        Point boxSizeVect = Point::Ones(boxSize);
+        Box domainBox = Box::Cube(domainSize);
+        ProblemDomain domain(domainBox, periodicity);
+        DisjointBoxLayout layout(domain, boxSizeVect);
 
-    double physDomainSize = 1.0;
-    //std::array<double, DIM> dx;
-    Array<double, DIM> dx;
-    dx.fill(physDomainSize / domainSize);
-    double dt = 0.5 / domainSize;
+        double physDomainSize = 1.0;
+        //std::array<double, DIM> dx;
+        Array<double, DIM> dx;
+        dx.fill(physDomainSize / domainSize);
+        double dt = 0.5 / domainSize;
 
-    pout() << setw(50) << setfill('=') << "=" << std::endl;
-    pout() << "Coarsest Level Parameters" << std::endl;
-    pout() << setw(50) << setfill('-') << "-" << std::endl;
-    pout() << "dx: " << dx[0] << " | dt: " << dt << std::endl;
-    pout() << "domain: " << domainBox << std::endl;
-    pout() << setw(50) << setfill('-') << "-" << std::endl << std::endl;
+        pout() << setw(50) << setfill('=') << "=" << std::endl;
+        pout() << "Coarsest Level Parameters" << std::endl;
+        pout() << setw(50) << setfill('-') << "-" << std::endl;
+        pout() << "dx: " << dx[0] << " | dt: " << dt << std::endl;
+        pout() << "domain: " << domainBox << std::endl;
+        pout() << setw(50) << setfill('-') << "-" << std::endl << std::endl;
 
-    std::vector<Point> refRatios(numLevels-1, Point::Ones(refRatio));
-    AMRGrid grid(layout, refRatios, numLevels);
+        std::vector<Point> refRatios(numLevels-1, Point::Ones(refRatio));
+        AMRGrid grid(layout, refRatios, numLevels);
 
-    double dxLevel = dx[0];
-    Point bufferSize = Point::Ones(regridBufferSize);
-    for (int lvl = 0; lvl < numLevels-1; lvl++)
-    {
-        LevelBoxData<double, NUMCOMPS> initData(grid[lvl], Point::Zeros());
-        //initData.initConvolve(f_advectionExact, dxLevel, t0);
-        Operator::initConvolve(initData, f_advectionExact, dxLevel, t0);
-        LevelTagData tags(grid[lvl], bufferSize);
-        for (auto iter = grid[lvl].begin(); iter.ok(); ++iter)
-        {
-            OP::generateTags(tags[*iter], initData[*iter]);
-        }
-        AMRGrid::buffer(tags, bufferSize);
-        grid.regrid(tags, lvl);
-        dxLevel /= refRatio;
-    }
-    
-    for (int lvl = 2; lvl < numLevels; lvl++)
-    {
-        grid.enforceNesting2(lvl);
-    }
+        double dxLevel = dx[0];
+        Point bufferSize = Point::Ones(regridBufferSize);
+        if (numLevels > 1)
+          {
+            for (int lvl = 0; lvl < numLevels-1; lvl++)
+              {
+                LevelBoxData<double, NUMCOMPS> initData(grid[lvl], Point::Zeros());
+                Operator::initConvolve(initData, f_advectionExact, dxLevel, t0);
+                LevelTagData tags(grid[lvl], bufferSize);
+                for (auto iter = grid[lvl].begin(); iter.ok(); ++iter)
+                  {
+                    OP::generateTags(tags[*iter], initData[*iter]);
+                  }
+                AMRGrid::buffer(tags, bufferSize);
+                grid.regrid(tags, lvl);
+                dxLevel /= refRatio;
+              }
+        
+            for (int lvl = 2; lvl < numLevels; lvl++)
+              {
+                grid.enforceNesting2(lvl);
+              }
+          }
+        AMRData<double, NUMCOMPS> U(grid, OP::ghost());
+        Operator::initConvolve(U, dx[0], f_advectionExact, t0);
+        U.averageDown();
+        AMRRK4<OP, double, NUMCOMPS> advectionOp(U, dx);
+        double sum0 = U[0].integrate(dx[0]);
+        pout() << "Initial level 0 conservation sum: " << sum0 << std::endl;
 
-    AMRData<double, NUMCOMPS> U(grid, OP::ghost());
-    //U.initConvolve(dx[0], f_advectionExact, t0);
-    Operator::initConvolve(U, dx[0], f_advectionExact, t0);
-    U.averageDown();
-    AMRRK4<OP, double, NUMCOMPS> advectionOp(U, dx);
-    double sum0 = U[0].integrate(dx[0]);
-    pout() << "Initial level 0 conservation sum: " << sum0 << std::endl;
-
-    double time = t0;
-#ifdef PR_HDF5
-    h5.setTime(time);
-    h5.setTimestep(dt);
-    h5.writeAMRData(dx, U, "U_N0");
-#endif
-    for (int k = 0; ((k < maxTimesteps) && (time < maxTime)); k++)
-    {
-        TIME_STEP = k;
-        PLOT_NUM = 0;      
-        advectionOp.advance(dt);
-        time += dt;
+        double time = t0;
 #ifdef PR_HDF5
         h5.setTime(time);
+        h5.setTimestep(dt);
+        h5.writeAMRData(dx, U, "U_"+to_string(domainSize)+"_N0");
 #endif
-        if ((k+1) % outputInterval == 0)
-        {
-            if (Proto::procID() == 0)
+
+        {      
+          struct rusage usage;
+          getrusage(RUSAGE_SELF, &usage);
+          if (Proto::procID() == 0)
             {
-                std::cout << "n: " << k << " | time: " << time << std::endl;
+              cout << "memory HWM before evolution: " << usage.ru_maxrss << endl;
+              int step = 0;
+              sizeout << step << " " << usage.ru_maxrss << endl;
             }
-#ifdef PR_HDF5
-            h5.writeAMRData(dx, U, "U_N%i", k+1);
-#endif
+          // pout() << "memory HWM before evolution: " << usage.ru_maxrss << endl;
         }
-    }
+        for (int k = 0; ((k < maxTimesteps) && (time < maxTime)); k++)
+          {
+            TIME_STEP = k;
+            PLOT_NUM = 0;
+         
+            advectionOp.advance(dt);
 
-    U.averageDown();
-    double sumFinal = U[0].integrate(dx[0]);
-    {
-      //cout << "Final level 0 conservation sum: " << sumFinal << std::endl;   
-      cout << "Conservation error: " << sumFinal-sum0 << std::endl;
-    }
-    AMRData<double, NUMCOMPS> USoln(U.grid(), Point::Zeros());
-    //USoln.initConvolve(dx[0], f_advectionExact, time);
-    Operator::initConvolve(USoln, dx[0], f_advectionExact, time);
+            // Memory HWM.
+        
+            struct rusage usage;
+            getrusage(RUSAGE_SELF, &usage);
+            if (Proto::procID() == 0)
+              {
+                sizeout << k << " " << usage.ru_maxrss << endl;
+              }      
+            time += dt;
 #ifdef PR_HDF5
-    h5.writeAMRData(dx, USoln, "USoln");
+            h5.setTime(time);
 #endif
+            if ((k+1) % outputInterval == 0)
+              {
+                int numBoxes = 0;
+                for (int ll = 0; ll < U.numLevels();ll++)
+                  {
+                    numBoxes += U[ll].size();
+                  }
+                if ((Proto::procID() == 0)&&(((k+1) % outputInterval) == 0 ))
+                  {
+                    std::cout << "n: " << k+1 << " |  time: " << time << 
+                      "  |  number of Boxes: " << numBoxes << std::endl;
+                  }
+#ifdef PR_HDF5             
+                h5.writeAMRData(dx, U, "U_"+to_string(domainSize)+"_N"+to_string(k+1));
+#endif
+              }        
+          }
+        {
+          struct rusage usage;
+          getrusage(RUSAGE_SELF, &usage);
+          if (Proto::procID() == 0)
+            cout << "memory HWM after evolution: " << usage.ru_maxrss << endl;
+          // pout() << "memory HWM after evolution: " << usage.ru_maxrss << endl;
+        }
+        U.averageDown();
+        double sumFinal = U[0].integrate(dx[0]);
+        consError = (sumFinal - sum0)/sum0;
+        if (Proto::procID() == 0)
+          {
+            //cout << "Final level 0 conservation sum: " << sumFinal << std::endl;   
+            cout << "Conservation error: " << sumFinal-sum0 << std::endl;
+            
+          }
+        AMRData<double, NUMCOMPS> USoln(U.grid(), Point::Zeros());
+        Operator::initConvolve(USoln, dx[0], f_advectionExact, time);
 
-    LevelBoxData<double, NUMCOMPS> UError(U.grid()[0], Point::Zeros());
-    U[0].copyTo(UError);
-    UError.increment(USoln[0], -1);
+        AMRData<double, NUMCOMPS> UError(U.grid(), Point::Zeros());
+        U.copyTo(UError);
+        UError.increment(USoln, -1);
+    
 #ifdef PR_HDF5
-    h5.writeLevel(dx, UError, "UError");
+        h5.writeAMRData(dx, UError, "UError");
 #endif
-    double error = UError.absMax();
-    std::cout << "error: " << error << std::endl;
-    error /= dt;
-    std::cout << "error/dt: " << error << std::endl;
-
+        double error = UError.integrateAbs(dx)/U.absMax();
+        errorRefs.push_back(error);
+        if (Proto::procID() == 0)
+          {
+            std::cout << "error: " << error << std::endl;
+            std::cout << " time: " << time << std::endl;
+          }
+        domainSize *=2;
+        dt *= .5;
+        maxTimesteps *= 2;
+      }
+    if (Proto::procID() == 0)
+      std::cout << "Relative conservation error (must be < 1.e-14 to pass): "
+                << fabs(consError) << std::endl;
+    if (maxRefs > 1)
+      {
+        double rate = log(errorRefs[maxRefs-2]/errorRefs[maxRefs-1])/log(2.0);
+        if (Proto::procID() == 0)
+          {
+            std::cout << "L1 error exponent (must be >= 4 to pass): " << rate << std::endl;         
+          }
+      }
     PR_TIMER_REPORT();
 #ifdef PR_MPI
     MPI_Finalize();
 #endif
 }
-
