bb5500fde7bd6ba40de4ffebd576e9d6b3ec236a
Chris <cgebhart@lbl.gov>
2023-10-19 11:33:50 -0700

    Added MBLevelRK4, the MMB analog of LevelRK4. Creating this class required a small refactor of MBLevelOp to add the BC template parameter I had previously neglected. A simple test file compiles and allows construction of an MBLevelRK4 object with no errors

---
+++ tests/MBLevelRK4Tests.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include "Proto.H"
+#include "Lambdas.H"
+#include "MBMap_Shear.H"
+#include "BoxOp_MBLaplace.H"
+
+using namespace Proto;
+
+TEST(MBLevelRK4Tests, Construction) {
+    int domainSize = 8;
+    int boxSize = 8;
+    HDF5Handler h5;
+
+    auto domain = buildShear(domainSize);
+    Point boxSizeVect = Point::Ones(boxSize);
+    MBDisjointBoxLayout layout(domain, boxSizeVect);
+
+    Array<Point, DIM+1> ghost;
+    ghost.fill(Point::Ones(4));
+    ghost[0] = Point::Ones(1);
+
+    // initialize map
+    MBLevelMap<MBMap_Shear, HOST> map;
+    map.define(layout, ghost);
+
+    MBLevelRK4<BoxOp_MBLaplace, MBMap_Shear, double> rk4(map);
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef PR_MPI
+    MPI_Init(&argc, &argv);
+#endif
+    int result = RUN_ALL_TESTS();
+#ifdef PR_MPI
+    MPI_Finalize();
+#endif
+    return result;
+}
