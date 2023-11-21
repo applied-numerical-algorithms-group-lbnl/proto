e8e83837e317bf2e77904d1ecb8b7894c4654eb5
Daniel Waters <dwaters@lbl.gov>
2023-11-20 16:45:53 -0800

    variadic foreach functions

--- experimental/forall_prototypes/BufferData.cpp
+++ experimental/forall_prototypes/BufferData.cpp
@@ -4,13 +4,20 @@ int main() {
 	Box bx = Box::Cube(5);
 	BufferData<double> start(bx);
 	start.iota();
+	start.print(); 
+	foreach(first<double>(),start,2.3);
+	start.print(); 
+	foreach(other<double>(),start,2.3,1.2);
+	start.print(); 
 	SimpleStencil<double> easy;
 	vector<BufferData<double>*> bdvec((bx.size(0)+1)/2);
 	bdvec[0] = &start;
 	for (int i=1; i<bdvec.size(); i++) 
 		bdvec[i] = new BufferData<double>{easy.apply(*(bdvec[i-1]))}; 
 	cout << "Initial buffer: " << endl;
-	for (int i=0; i<bdvec.size(); i++)
-		bdvec[i]->print();
+	for (int i=0; i<bdvec.size(); i++) 
+		bdvec[i]->print(); 
+	foreach(both<double>(),*bdvec[0],*bdvec[1],4.1);
+	bdvec[1]->print();
 	return 0;
 }
