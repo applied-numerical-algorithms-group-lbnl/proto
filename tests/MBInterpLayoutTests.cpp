aa0427a3e0684919c3248c6e0a76cbadb5eba848
cgebhart <cgebhart@lbl.gov>
2025-01-17 08:50:35 -0800

    Added MBInterpLayoutTests  - Tests are currently just for visual checks. Working on proper tests.

---
+++ tests/MBInterpLayoutTests.cpp
@@ -0,0 +1,103 @@
+#include <gtest/gtest.h>
+#include "Proto.H"
+#include "TestFunctions.H"
+using namespace Proto;
+
+namespace {
+    MBDisjointBoxLayout testLayout(int boxSize, int domainSize, int numBlocks)
+    {
+        auto domain = buildXPoint(domainSize, numBlocks);
+        std::vector<MBPoint> patches;
+        std::vector<Point> boxSizes;
+        for (BlockIndex bi = 0; bi < numBlocks; bi++)
+        {
+            for (auto pi : Box::Cube(domainSize / boxSize))
+            {
+                if (pi != Point::X())
+                {
+                    patches.push_back(MBPoint(pi, bi));
+                }
+            }
+            boxSizes.push_back(Point::Ones(boxSize));
+        }
+        return MBDisjointBoxLayout(domain, patches, boxSizes);
+    }
+
+    BoxData<int> printInterpLayout(MBInterpLayout layout, Point center, Point boundaryDir, MBIndex index)
+    {
+        auto footprint = layout.footprint(center, boundaryDir, index);
+        Box b;
+        for (auto fi : footprint)
+        {
+            b &= (fi + center);
+        }
+        BoxData<int> data(b);
+        data.setVal(0);
+        for (auto fi : footprint)
+        {
+            data(fi + center) = 1.0;
+        }
+        data(center) = 2.0;
+        return data;
+    }
+}
+
+TEST(MBInterpLayout, BlockBoundary) {
+    int boxSize = 8;
+    int domainSize = boxSize*2;
+    int numBlocks = 4;
+
+    auto layout = testLayout(boxSize, domainSize, numBlocks);
+    layout.print();
+    MBInterpLayout interpLayout(layout);
+
+    for (auto pi : Box::Kernel(2))
+    {
+        if (pi.sumAbs() <= 2) { interpLayout.addPoint(pi); }
+    }
+    MBLevelBoxData<int, 1, HOST> domainData(layout, Point::Zeros());
+    domainData.setVal(-1);
+#if PR_VERBOSE > 0
+    HDF5Handler h5;
+    h5.writeLevel(domainData.getBlock(0), "DOMAIN");
+#endif
+
+    int plotIter = 0;
+    for (auto iter : layout)
+    {
+        if (layout.block(iter) != 0)
+        {
+            continue;
+        }
+        Box patchBox = layout[iter];
+        for (auto dir : Box::Kernel(1))
+        {
+            if (dir == Point::Zeros())
+            {
+                continue;
+            }
+            Box boundBox = patchBox.adjacent(dir * 3);
+            for (auto center : boundBox)
+            {
+                if (layout.domain().inInterior(center, 0)) { continue; }
+                auto footprintData = printInterpLayout(interpLayout, center, dir, iter);
+#if PR_VERBOSE > 0
+                h5.writePatch(footprintData, "FOOTPRINT_%i", plotIter);
+                plotIter++;
+#endif
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef PR_MPI
+    MPI_Init(&argc, &argv);
+#endif
+    int result = RUN_ALL_TESTS();
+#ifdef PR_MPI
+    MPI_Finalize();
+#endif
+    return result;
+}
