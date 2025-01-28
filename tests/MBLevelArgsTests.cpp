90ca2e9f9b98018cc0a74be075ca2f5f310b5705
cgebhart <cgebhart@lbl.gov>
2025-01-28 14:14:41 -0800

    Removed DEVICE references from MBLevelArgsTests since cuda is acting up on my development machine

--- tests/MBLevelArgsTests.cpp
+++ tests/MBLevelArgsTests.cpp
@@ -15,7 +15,7 @@ TEST(MBLevelArgs, GetAndSet) {
 
     MBLevelArgs<MBMap_XPointRigid, HOST> args(map);
 
-    auto d0 = std::make_shared<MBLevelBoxData<double, 2, DEVICE>>(layout, Point::Ones());
+    auto d0 = std::make_shared<MBLevelBoxData<double, 2, HOST>>(layout, Point::Ones());
     auto d1 = std::make_shared<MBLevelBoxData<int, 3, HOST>>(layout, Point::Ones(2));
 
     int c0 = 17;
@@ -27,16 +27,16 @@ TEST(MBLevelArgs, GetAndSet) {
     args.Set("c0", c0);
     args.Set("c1", c1);
 
-    auto& d00 = args.Get<double, 2, DEVICE>("d0");
+    auto& d00 = args.Get<double, 2, HOST>("d0");
     auto& d11 = args.Get<int, 3, HOST>("d1");
     auto c00 = args.Get<int>("c0");
     auto c11 = args.Get<double>("c1");
 
-    // EXPECT_EQ(d0, &d00);
-    // EXPECT_EQ(d1, &d11);
+    EXPECT_EQ(d0.get(), &d00);
+    EXPECT_EQ(d1.get(), &d11);
 
-    // EXPECT_EQ(c0, c00);
-    // EXPECT_EQ(c1, c11);
+    EXPECT_EQ(c0, c00);
+    EXPECT_EQ(c1, c11);
 }
 
 int main(int argc, char *argv[]) {
