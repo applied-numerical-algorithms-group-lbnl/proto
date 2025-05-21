feec1a5164ee27f19c1469216cb9639fc306608a
cgebhart <cgebhart@lbl.gov>
2025-05-21 14:58:51 -0700

    Turned boundary condiditon test into a convergence test.

--- tests/BoundaryConditionTests.cpp
+++ tests/BoundaryConditionTests.cpp
@@ -80,6 +80,7 @@ TEST(BoundaryCondition, ConstDirichletGhost)
     int numIter = 3;
     constexpr int numBlocks = 5;
 
+    double err[numIter];
     for (int nn = 0; nn < numIter; nn++)
     {
         auto domain = buildXPoint(domainSize, numBlocks);
@@ -118,28 +119,47 @@ TEST(BoundaryCondition, ConstDirichletGhost)
         Box domainBox = Box::Cube(domainSize);
         Box xBoundBox = domainBox.adjacent(-Point::X(),1);
         Box yBoundBox = domainBox.adjacent(-Point::Y(),1);
+        err[nn] = 0;
         for (auto iter : layout)
         {
             auto& data_i = data[iter];
             Face fx(0, Side::Lo);
             Face fy(1,Side::Lo);
             BoundaryCondition::DirichletFillGhost<double>(data_i, 0, fx, domainBox);
-            BoundaryCondition::DirichletFillGhost<double>(data_i, 0, fy, domainBox);
+            //BoundaryCondition::DirichletFillGhost<double>(data_i, 0, fy, domainBox);
             if (!(data_i.box() & xBoundBox).empty())
             {
                 BoxData<double, 1> boundaryValues(layout[iter].adjacent(-Point::X()));
                 boundaryValues |= Stencil<double>::CellToFace(0)(data_i);
-                std::cout << "boundary interp value: " << boundaryValues.absMax() << std::endl;
+                err[nn] = max(err[nn], boundaryValues.absMax());
             }
+            // if (!(data_i.box() & yBoundBox).empty())
+            // {
+            //     BoxData<double, 1> boundaryValues(layout[iter].adjacent(-Point::Y()));
+            //     boundaryValues |= Stencil<double>::CellToFace(1)(data_i);
+            //     err[nn] = max(err[nn], boundaryValues.absMax());
+            // }
         }
+        // Reduction<double, Abs> rxn;
+        // rxn.reduce(&err[nn], sizeof(double));
+        // err[nn] = rxn.fetch();
+
         #if PR_VERBOSE > 0
         h5.writeMBLevel(data, "DIRICHLET_GHOST_DATA_1");
+        std::cout << "Error: " << err[nn] << std::endl;
         #endif
 
         domainSize *= 2;
         boxSize *= 2;
     }
-    
+    for (int ii = 1; ii < numIter; ii++)
+    {
+        double rate = log(err[ii-1]/err[ii])/log(2.0);
+        #if PR_VERBOSE > 0
+        std::cout << "Convergence Rate: " << rate << std::endl;
+        #endif
+        EXPECT_GT(rate, 3.9);
+    }
 
 }
 int main(int argc, char *argv[]) {
