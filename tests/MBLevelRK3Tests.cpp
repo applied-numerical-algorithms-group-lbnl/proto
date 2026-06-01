e4a1e8393dfd18b13e88ae3c0f9625031564d986
dhami1234 <dhami1234@gmail.com>
2026-06-01 12:05:14 -0400

    RK3 implementation

---
+++ tests/MBLevelRK3Tests.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include "Proto.H"
+#include "TestFunctions.H"
+#include "MBMap_Shear.H"
+#include "BoxOp_MBLaplace.H"
+
+using namespace Proto;
+
+TEST(MBLevelRK3Tests, Construction) {
+    int domainSize = 8;
+    int boxSize = 8;
+
+    auto domain = buildShear(domainSize);
+    Point boxSizeVect = Point::Ones(boxSize);
+    MBDisjointBoxLayout layout(domain, boxSizeVect);
+
+    Array<Point, DIM+1> ghost;
+    ghost.fill(Point::Ones(4));
+    ghost[0] = Point::Ones(1);
+
+    // initialize map
+    MBLevelMap<MBMap_Shear, HOST> map;
+    map.define(layout, ghost);
+    map.initialize();
+
+    MBInterpOp interp(map, Point::Ones(4), 4);
+    MBLevelRK3<BoxOp_MBLaplace, MBMap_Shear, double> rk3(map, interp);
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef PR_MPI
+    MPI_Init(&argc, &argv);
+#endif
+    int result = RUN_ALL_TESTS();
+#ifdef PR_MPI
+    MPI_Finalize();
+#endif
+    return result;
+}
